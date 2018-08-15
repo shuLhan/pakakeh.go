e24219bc6182db694dfe1ca6ab1c8b9e342b32c2
Shulhan <ms@kilabit.info>
2018-08-16 04:12:12 +0700

    [chore] Add copyright notice on root directory (share)

--- share.go
+++ share.go
@@ -1,3 +1,7 @@
+// Copyright 2018, Shulhan <ms@kilabit.info>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 //
 // Package share contains common tools and library for working with Go
 // language.
