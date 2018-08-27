282d9be7a49773a438713859a646ffc5c8c2e1a4
Shulhan <ms@kilabit.info>
2018-08-28 01:42:04 +0700

    lib/net: parse domain and options value

--- lib/net/resolvconf.go
+++ lib/net/resolvconf.go
@@ -6,36 +6,140 @@ package net
 
 import (
 	"bytes"
+	"os"
+	"strconv"
+	"strings"
 
-	"github.com/shuLhan/share/lib/file"
+	libfile "github.com/shuLhan/share/lib/file"
 )
 
-type ResolvConf struct {
-	SearchList  []string
-	NameServers []string
-}
+const (
+	envLocaldomain = "LOCALDOMAIN"
+)
 
 var (
 	newLineTerms = []byte{'\n'}
 	spaceSeps    = []byte{'\t', '\n', '\v', '\f', '\r', ' '}
 )
 
+var getHostname = func() (string, error) {
+	return os.Hostname()
+}
+
+//
+// ResolvConf contains value of resolver configuration file.
+//
+// Reference: "man resolv.conf" in Linux.
+//
+type ResolvConf struct {
+	// Local domain name.
+	// Most queries for names within this domain can use short names
+	// relative to the local domain.  If set to '.', the root domain
+	// is considered.  If no domain entry is present, the domain is
+	// determined from the local hostname returned by gethostname(2);
+	// the domain part is taken to be everything after the first '.'.
+	// Finally, if the hostname does not contain a domain part, the
+	// root domain is assumed.
+	Domain string
+
+	// Search list for host-name lookup.
+	// The search list is normally determined from the local domain
+	// name; by default, it contains only the local domain name.
+	// This may be changed by listing the desired domain search path
+	// following the search keyword with spaces or tabs separating
+	// the names.  Resolver queries having fewer than ndots dots
+	// (default is 1) in them will be attempted using each component
+	// of the search path in turn until a match is found.  For
+	// environments with multiple subdomains please read options
+	// ndots:n below to avoid man-in-the-middle attacks and
+	// unnecessary traffic for the root-dns-servers.  Note that this
+	// process may be slow and will generate a lot of network traffic
+	// if the servers for the listed domains are not local, and that
+	// queries will time out if no server is available for one of the
+	// domains.
+	//
+	// The search list is currently limited to six domains with a
+	// total of 256 characters.
+	Search []string
+
+	// Name server IP address
+	// Internet address of a name server that the resolver should
+	// query, either an IPv4 address (in dot notation), or an IPv6
+	// address in colon (and possibly dot) notation as per RFC 2373.
+	// Up to 3 name servers may be listed, one per keyword.  If there are
+	// multiple servers, the resolver library queries them in the order
+	// listed.  If no nameserver entries are present, the default is to
+	// use the name server on the local machine.  (The algorithm used is
+	// to try a name server, and if the query times out, try the next,
+	// until out of name servers, then repeat trying all the name servers
+	// until a maximum number of retries are made.)
+	NameServers []string
+
+	// Sets a threshold for the number of dots which must appear in a name
+	// before an initial absolute query will be made.  The default for n
+	// is 1, meaning that if there are any dots in a name, the name will
+	// be tried first as an absolute name before any search list elements
+	// are appended to it.  The value for this option is silently capped
+	// to 15.
+	NDots int
+
+	// Sets the amount of time the resolver will wait for a response from
+	// a remote name server before retrying the query via a different name
+	// server. This may not be the total time taken by any resolver API
+	// call and there is no guarantee that a single resolver API call maps
+	// to a single timeout.  Measured in seconds, the default is 5 The
+	// value for this option is silently capped to 30.
+	Timeout int
+
+	// Sets the number of times the resolver will send a query to its name
+	// servers before giving up and returning an error to the calling
+	// application.  The default is 2. The value for this option is
+	// silently capped to 5.
+	Attempts int
+
+	// OptMisc contains other options with string key and boolean value.
+	OptMisc map[string]bool
+}
+
 //
 // NewResolvConf open resolv.conf file in path and return the parsed records.
 //
 func NewResolvConf(path string) (*ResolvConf, error) {
-	rc := &ResolvConf{}
+	rc := &ResolvConf{
+		OptMisc: make(map[string]bool),
+	}
 
-	err := rc.parse(path)
+	reader, err := libfile.NewReader(path)
 	if err != nil {
 		return nil, err
 	}
+
+	rc.parse(reader)
 
 	return rc, nil
 }
 
 //
-// parse open and parse the resolv.conf file.a
+// Init parse resolv.conf from string.
+//
+func (rc *ResolvConf) Init(src string) {
+	reader := new(libfile.Reader)
+	reader.Init(src)
+
+	rc.reset()
+
+	rc.parse(reader)
+}
+
+func (rc *ResolvConf) reset() {
+	rc.Domain = ""
+	rc.Search = nil
+	rc.NameServers = nil
+	rc.OptMisc = make(map[string]bool)
+}
+
+//
+// parse open and parse the resolv.conf file.
 //
 // Lines that contain a semicolon (;) or hash character (#) in the first
 // column are treated as comments.
@@ -46,12 +150,7 @@ func NewResolvConf(path string) (*ResolvConf, error) {
 //
 // See `man resolv.conf`
 //
-func (rc *ResolvConf) parse(path string) error {
-	reader, err := file.NewReader(path)
-	if err != nil {
-		return err
-	}
-
+func (rc *ResolvConf) parse(reader *libfile.Reader) {
 	for {
 		c := reader.SkipSpace()
 		if c == 0 {
@@ -68,22 +167,56 @@ func (rc *ResolvConf) parse(path string) error {
 			continue
 		}
 
-		key := string(bytes.ToLower(tok))
-		println("domain:", key)
-		switch key {
-		case "domain", "search":
-			rc.parseValue(reader, &rc.SearchList)
+		v := string(bytes.ToLower(tok))
+		switch v {
+		case "domain":
+			rc.parseValue(reader, &rc.Domain)
+		case "search":
+			rc.parseSearch(reader)
 		case "nameserver":
-			rc.parseValue(reader, &rc.NameServers)
+			v = ""
+			rc.parseValue(reader, &v)
+			if len(rc.NameServers) < 3 && len(v) > 0 {
+				rc.NameServers = append(rc.NameServers, v)
+			}
+		case "options":
+			rc.parseOptions(reader)
 		default:
 			reader.SkipUntil(newLineTerms)
 		}
 	}
 
-	return nil
+	rc.sanitize()
 }
 
-func (rc *ResolvConf) parseValue(reader *file.Reader, out *[]string) {
+func (rc *ResolvConf) parseValue(reader *libfile.Reader, out *string) {
+	c := reader.SkipHorizontalSpace()
+	if c == '\n' || c == 0 {
+		return
+	}
+
+	tok, isTerm := reader.ReadUntil(spaceSeps, newLineTerms)
+	if len(tok) > 0 {
+		*out = string(tok)
+	}
+
+	if !isTerm {
+		reader.SkipUntil(newLineTerms)
+	}
+}
+
+//
+// (1) The domain and search keywords are mutually exclusive.  If more than
+// one instance of these keywords is present, the last instance wins.
+//
+func (rc *ResolvConf) parseSearch(reader *libfile.Reader) {
+	max := 6
+	maxLen := 255
+	var curLen int
+
+	// (1)
+	rc.Search = nil
+
 	for {
 		c := reader.SkipHorizontalSpace()
 		if c == '\n' || c == 0 {
@@ -92,10 +225,108 @@ func (rc *ResolvConf) parseValue(reader *file.Reader, out *[]string) {
 
 		tok, isTerm := reader.ReadUntil(spaceSeps, newLineTerms)
 		if len(tok) > 0 {
-			*out = append(*out, string(tok))
+			if curLen+len(tok) > maxLen {
+				break
+			}
+
+			rc.Search = append(rc.Search, string(tok))
+			if len(rc.Search) == max {
+				break
+			}
+
+			curLen += len(tok)
 		}
 		if isTerm {
 			break
 		}
+	}
+
+	reader.SkipUntil(newLineTerms)
+}
+
+func (rc *ResolvConf) parseOptions(reader *libfile.Reader) {
+	for {
+		c := reader.SkipHorizontalSpace()
+		if c == '\n' || c == 0 {
+			break
+		}
+
+		tok, isTerm := reader.ReadUntil(spaceSeps, newLineTerms)
+		if len(tok) > 0 {
+			rc.parseOptionsKV(tok)
+		}
+		if isTerm {
+			break
+		}
+	}
+}
+
+func (rc *ResolvConf) parseOptionsKV(opt []byte) {
+	var k, v []byte
+	for x := 0; x < len(opt); x++ {
+		if opt[x] == ':' {
+			k = opt[:x]
+			if x+1 < len(opt) {
+				v = opt[x+1:]
+			}
+			break
+		}
+	}
+	if len(k) == 0 {
+		k = opt
+	}
+
+	sk := string(k)
+	switch sk {
+	case "ndots":
+		rc.NDots, _ = strconv.Atoi(string(v))
+	case "timeout":
+		rc.Timeout, _ = strconv.Atoi(string(v))
+	case "attempts":
+		rc.Attempts, _ = strconv.Atoi(string(v))
+	default:
+		if len(k) > 0 {
+			rc.OptMisc[sk] = true
+		}
+	}
+}
+
+func (rc *ResolvConf) sanitize() {
+	// Sanitize domain name
+	if len(rc.Domain) == 0 {
+		rc.Domain, _ = getHostname()
+	}
+	if len(rc.Domain) > 0 {
+		names := strings.Split(rc.Domain, ".")
+		if len(names) > 1 {
+			rc.Domain = strings.Join(names[1:], ".")
+		}
+	}
+
+	// The search keyword of a system's resolv.conf file can be overridden
+	// on a per-process basis by setting the environment variable
+	// LOCALDOMAIN to a space-separated list of search domains.
+	envLocalDomain := os.Getenv(envLocaldomain)
+	if len(envLocalDomain) > 0 {
+		rc.Search = strings.Split(envLocalDomain, " ")
+		if len(rc.Search) > 6 {
+			rc.Search = rc.Search[:6]
+		}
+	}
+
+	if rc.NDots == 0 {
+		rc.NDots = 1
+	} else if rc.NDots > 15 {
+		rc.NDots = 15
+	}
+	if rc.Timeout == 0 {
+		rc.Timeout = 5
+	} else if rc.Timeout > 30 {
+		rc.Timeout = 30
+	}
+	if rc.Attempts == 0 {
+		rc.Attempts = 2
+	} else if rc.Attempts > 5 {
+		rc.Attempts = 5
 	}
 }
--- lib/net/resolvconf_test.go
+++ lib/net/resolvconf_test.go
@@ -5,29 +5,193 @@
 package net
 
 import (
+	"os"
 	"testing"
 
 	"github.com/shuLhan/share/lib/test"
+	libtext "github.com/shuLhan/share/lib/text"
 )
 
 func TestNewResolvConf(t *testing.T) {
 	cases := []struct {
-		path           string
-		expSearchList  []string
-		expNameServers []string
+		desc   string
+		path   string
+		exp    *ResolvConf
+		expErr string
 	}{{
-		path:           "testdata/resolv.conf",
-		expSearchList:  []string{"a", "b", "c", "d", "e", "f"},
-		expNameServers: []string{"127.0.0.1", "1.1.1.1", "2.2.2.2"},
+		desc:   "With invalid file",
+		path:   "",
+		exp:    nil,
+		expErr: "open : no such file or directory",
+	}, {
+		desc: "From testdata/resolv.conf",
+		path: "testdata/resolv.conf",
+		exp: &ResolvConf{
+			Domain:      "a",
+			Search:      []string{"d", "e", "f", "g", "h", "i"},
+			NameServers: []string{"127.0.0.1", "1.1.1.1", "2.2.2.2"},
+			NDots:       1,
+			Timeout:     5,
+			Attempts:    2,
+			OptMisc:     make(map[string]bool),
+		},
 	}}
 
 	for _, c := range cases {
+		t.Log(c.desc)
+
 		rc, err := NewResolvConf(c.path)
 		if err != nil {
-			t.Fatal(err)
+			test.Assert(t, "expError", c.expErr, err.Error(), true)
 		}
 
-		test.Assert(t, "SearchList", c.expSearchList, rc.SearchList, true)
-		test.Assert(t, "NameServers", c.expNameServers, rc.NameServers, true)
+		test.Assert(t, "exp", c.exp, rc, true)
+	}
+}
+
+func TestResolvConf_Init(t *testing.T) {
+	getHostname = func() (string, error) {
+		return "kilabit.info", nil
+	}
+
+	veryLongName := string(libtext.Random([]byte(libtext.ASCIILetters), 255))
+
+	cases := []struct {
+		desc           string
+		src            string
+		envLocaldomain string
+		exp            *ResolvConf
+	}{{
+		desc: "Duplicate domain",
+		src: `
+domain a
+domain b
+`,
+		exp: &ResolvConf{
+			Domain:   "b",
+			NDots:    1,
+			Timeout:  5,
+			Attempts: 2,
+			OptMisc:  make(map[string]bool),
+		},
+	}, {
+		desc: "Without domain",
+		src: `
+search a b c
+`,
+		exp: &ResolvConf{
+			Domain:   "info",
+			Search:   []string{"a", "b", "c"},
+			NDots:    1,
+			Timeout:  5,
+			Attempts: 2,
+			OptMisc:  make(map[string]bool),
+		},
+	}, {
+		desc: "Duplicate search",
+		src: `
+search q w e r t y u
+search a b c d e f g
+`,
+		exp: &ResolvConf{
+			Domain:   "info",
+			Search:   []string{"a", "b", "c", "d", "e", "f"},
+			NDots:    1,
+			Timeout:  5,
+			Attempts: 2,
+			OptMisc:  make(map[string]bool),
+		},
+	}, {
+		desc: "More than 3 nameservers",
+		src: `
+nameserver 1
+nameserver 2
+nameserver 3
+nameserver 4
+`,
+		exp: &ResolvConf{
+			Domain:      "info",
+			NameServers: []string{"1", "2", "3"},
+			NDots:       1,
+			Timeout:     5,
+			Attempts:    2,
+			OptMisc:     make(map[string]bool),
+		},
+	}, {
+		desc: "A very long search domain > 255 chars",
+		src:  `search aaaaa bbbbb ccccc ddddd ` + veryLongName,
+		exp: &ResolvConf{
+			Domain:   "info",
+			Search:   []string{"aaaaa", "bbbbb", "ccccc", "ddddd"},
+			NDots:    1,
+			Timeout:  5,
+			Attempts: 2,
+			OptMisc:  make(map[string]bool),
+		},
+	}, {
+		desc:           "Overriding search with env",
+		envLocaldomain: "a b c d e f g h",
+		exp: &ResolvConf{
+			Domain:   "info",
+			Search:   []string{"a", "b", "c", "d", "e", "f"},
+			NDots:    1,
+			Timeout:  5,
+			Attempts: 2,
+			OptMisc:  make(map[string]bool),
+		},
+	}, {
+		desc: "Single line options",
+		src:  `options debug ndots:3 timeout:31 attempts:44`,
+		exp: &ResolvConf{
+			Domain:   "info",
+			NDots:    3,
+			Timeout:  30,
+			Attempts: 5,
+			OptMisc: map[string]bool{
+				"debug": true,
+			},
+		},
+	}, {
+		desc: "Multi lines options",
+		src: `
+options debug
+options ndots:3
+options timeout:31
+options attempts:44
+options ndots:33
+`,
+		exp: &ResolvConf{
+			Domain:   "info",
+			NDots:    15,
+			Timeout:  30,
+			Attempts: 5,
+			OptMisc: map[string]bool{
+				"debug": true,
+			},
+		},
+	}}
+
+	rc := new(ResolvConf)
+
+	for _, c := range cases {
+		t.Log(c.desc)
+
+		if len(c.envLocaldomain) > 0 {
+			err := os.Setenv(envLocaldomain, c.envLocaldomain)
+			if err != nil {
+				t.Fatal(err)
+			}
+		}
+
+		rc.Init(c.src)
+
+		if len(c.envLocaldomain) > 0 {
+			err := os.Unsetenv(envLocaldomain)
+			if err != nil {
+				t.Fatal(err)
+			}
+		}
+
+		test.Assert(t, "ResolvConf", c.exp, rc, true)
 	}
 }
