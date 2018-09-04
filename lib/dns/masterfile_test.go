1d33be7cb40f0f7c3e378f46eb188a5c4627d1e4
Shulhan <ms@kilabit.info>
2018-09-04 22:50:08 +0700

    lib/dns: add another test for parsing master file

--- lib/dns/masterfile_test.go
+++ lib/dns/masterfile_test.go
@@ -138,7 +138,7 @@ func TestMasterInitRFC1035(t *testing.T) {
 		expErr error
 		exp    []*Message
 	}{{
-		desc:   "",
+		desc:   "RFC1035 section 5.3",
 		origin: "ISI.EDU.",
 		ttl:    3600,
 		in: `
@@ -331,9 +331,6 @@ VAXA    A       10.2.0.27
 			libtest.Assert(t, "Message.Question", c.exp[x].Question, msg.Question, true)
 
 			for y, answer := range msg.Answer {
-				t.Logf("Expecting answer rdata: %s\n", c.exp[x].Answer[y].RData())
-				t.Logf("Got answer rdata: %s\n", answer.RData())
-
 				libtest.Assert(t, "Answer.Name", c.exp[x].Answer[y].Name, answer.Name, true)
 				libtest.Assert(t, "Answer.Type", c.exp[x].Answer[y].Type, answer.Type, true)
 				libtest.Assert(t, "Answer.Class", c.exp[x].Answer[y].Class, answer.Class, true)
@@ -356,4 +353,242 @@ VAXA    A       10.2.0.27
 			}
 		}
 	}
+}
+
+func TestMasterInit2(t *testing.T) {
+	cases := []struct {
+		desc   string
+		origin string
+		ttl    uint32
+		in     string
+		expErr error
+		exp    []*Message
+	}{{
+		desc: "From http://www.tcpipguide.com/free/t_DNSMasterFileFormat-4.htm",
+		in: `
+$ORIGIN pcguide.com.
+@ IN SOA ns23.pair.com. root.pair.com. (
+2001072300 ; Serial
+3600 ; Refresh
+300 ; Retry
+604800 ; Expire
+3600 ) ; Minimum
+
+@ IN NS ns23.pair.com.
+@ IN NS ns0.ns0.com.
+
+localhost IN A 127.0.0.1
+@ IN A 209.68.14.80
+  IN MX 50 qs939.pair.com.
+
+www IN CNAME @
+ftp IN CNAME @
+mail IN CNAME @
+relay IN CNAME relay.pair.com.
+`,
+		exp: []*Message{{
+			Header: &SectionHeader{},
+			Question: &SectionQuestion{
+				Name:  []byte("pcguide.com."),
+				Type:  QueryTypeSOA,
+				Class: QueryClassIN,
+			},
+			Answer: []*ResourceRecord{{
+				Name:  []byte("pcguide.com."),
+				Type:  QueryTypeSOA,
+				Class: QueryClassIN,
+				TTL:   3600,
+				SOA: &RDataSOA{
+					MName:   []byte("ns23.pair.com."),
+					RName:   []byte("root.pair.com."),
+					Serial:  2001072300,
+					Refresh: 3600,
+					Retry:   300,
+					Expire:  604800,
+					Minimum: 3600,
+				},
+			}},
+		}, {
+			Header: &SectionHeader{},
+			Question: &SectionQuestion{
+				Name:  []byte("pcguide.com."),
+				Type:  QueryTypeNS,
+				Class: QueryClassIN,
+			},
+			Answer: []*ResourceRecord{{
+				Name:  []byte("pcguide.com."),
+				Type:  QueryTypeNS,
+				Class: QueryClassIN,
+				TTL:   3600,
+				Text: &RDataText{
+					v: []byte("ns23.pair.com."),
+				},
+			}, {
+				Name:  []byte("pcguide.com."),
+				Type:  QueryTypeNS,
+				Class: QueryClassIN,
+				TTL:   3600,
+				Text: &RDataText{
+					v: []byte("ns0.ns0.com."),
+				},
+			}},
+		}, {
+			Header: &SectionHeader{},
+			Question: &SectionQuestion{
+				Name:  []byte("localhost.pcguide.com."),
+				Type:  QueryTypeA,
+				Class: QueryClassIN,
+			},
+			Answer: []*ResourceRecord{{
+				Name:  []byte("localhost.pcguide.com."),
+				Type:  QueryTypeA,
+				Class: QueryClassIN,
+				TTL:   3600,
+				Text: &RDataText{
+					v: []byte("127.0.0.1"),
+				},
+			}},
+		}, {
+			Header: &SectionHeader{},
+			Question: &SectionQuestion{
+				Name:  []byte("pcguide.com."),
+				Type:  QueryTypeA,
+				Class: QueryClassIN,
+			},
+			Answer: []*ResourceRecord{{
+				Name:  []byte("pcguide.com."),
+				Type:  QueryTypeA,
+				Class: QueryClassIN,
+				TTL:   3600,
+				Text: &RDataText{
+					v: []byte("209.68.14.80"),
+				},
+			}},
+		}, {
+			Header: &SectionHeader{},
+			Question: &SectionQuestion{
+				Name:  []byte("pcguide.com."),
+				Type:  QueryTypeMX,
+				Class: QueryClassIN,
+			},
+			Answer: []*ResourceRecord{{
+				Name:  []byte("pcguide.com."),
+				Type:  QueryTypeMX,
+				Class: QueryClassIN,
+				TTL:   3600,
+				MX: &RDataMX{
+					Preference: 50,
+					Exchange:   []byte("qs939.pair.com."),
+				},
+			}},
+		}, {
+			Header: &SectionHeader{},
+			Question: &SectionQuestion{
+				Name:  []byte("www.pcguide.com."),
+				Type:  QueryTypeCNAME,
+				Class: QueryClassIN,
+			},
+			Answer: []*ResourceRecord{{
+				Name:  []byte("www.pcguide.com."),
+				Type:  QueryTypeCNAME,
+				Class: QueryClassIN,
+				TTL:   3600,
+				Text: &RDataText{
+					v: []byte("pcguide.com."),
+				},
+			}},
+		}, {
+			Header: &SectionHeader{},
+			Question: &SectionQuestion{
+				Name:  []byte("ftp.pcguide.com."),
+				Type:  QueryTypeCNAME,
+				Class: QueryClassIN,
+			},
+			Answer: []*ResourceRecord{{
+				Name:  []byte("ftp.pcguide.com."),
+				Type:  QueryTypeCNAME,
+				Class: QueryClassIN,
+				TTL:   3600,
+				Text: &RDataText{
+					v: []byte("pcguide.com."),
+				},
+			}},
+		}, {
+			Header: &SectionHeader{},
+			Question: &SectionQuestion{
+				Name:  []byte("mail.pcguide.com."),
+				Type:  QueryTypeCNAME,
+				Class: QueryClassIN,
+			},
+			Answer: []*ResourceRecord{{
+				Name:  []byte("mail.pcguide.com."),
+				Type:  QueryTypeCNAME,
+				Class: QueryClassIN,
+				TTL:   3600,
+				Text: &RDataText{
+					v: []byte("pcguide.com."),
+				},
+			}},
+		}, {
+			Header: &SectionHeader{},
+			Question: &SectionQuestion{
+				Name:  []byte("relay.pcguide.com."),
+				Type:  QueryTypeCNAME,
+				Class: QueryClassIN,
+			},
+			Answer: []*ResourceRecord{{
+				Name:  []byte("relay.pcguide.com."),
+				Type:  QueryTypeCNAME,
+				Class: QueryClassIN,
+				TTL:   3600,
+				Text: &RDataText{
+					v: []byte("relay.pair.com."),
+				},
+			}},
+		}},
+	}}
+
+	m := newMaster()
+
+	for _, c := range cases {
+		t.Log(c.desc)
+
+		m.Init(c.in, c.origin, c.ttl)
+
+		err := m.parse()
+		if err != nil {
+			libtest.Assert(t, "err", c.expErr, err.Error(), true)
+			continue
+		}
+
+		libtest.Assert(t, "messages length:", len(c.exp), len(m.msgs), true)
+
+		for x, msg := range m.msgs {
+			libtest.Assert(t, "Message.Header", c.exp[x].Header, msg.Header, true)
+			libtest.Assert(t, "Message.Question", c.exp[x].Question, msg.Question, true)
+
+			for y, answer := range msg.Answer {
+				libtest.Assert(t, "Answer.Name", c.exp[x].Answer[y].Name, answer.Name, true)
+				libtest.Assert(t, "Answer.Type", c.exp[x].Answer[y].Type, answer.Type, true)
+				libtest.Assert(t, "Answer.Class", c.exp[x].Answer[y].Class, answer.Class, true)
+				libtest.Assert(t, "Answer.TTL", c.exp[x].Answer[y].TTL, answer.TTL, true)
+				libtest.Assert(t, "Answer.RData()", c.exp[x].Answer[y].RData(), answer.RData(), true)
+			}
+			for y, auth := range msg.Authority {
+				libtest.Assert(t, "Authority.Name", c.exp[x].Authority[y].Name, auth.Name, true)
+				libtest.Assert(t, "Authority.Type", c.exp[x].Authority[y].Type, auth.Type, true)
+				libtest.Assert(t, "Authority.Class", c.exp[x].Authority[y].Class, auth.Class, true)
+				libtest.Assert(t, "Authority.TTL", c.exp[x].Authority[y].TTL, auth.TTL, true)
+				libtest.Assert(t, "Authority.RData()", c.exp[x].Authority[y].RData(), auth.RData(), true)
+			}
+			for y, add := range msg.Additional {
+				libtest.Assert(t, "Additional.Name", c.exp[x].Additional[y].Name, add.Name, true)
+				libtest.Assert(t, "Additional.Type", c.exp[x].Additional[y].Type, add.Type, true)
+				libtest.Assert(t, "Additional.Class", c.exp[x].Additional[y].Class, add.Class, true)
+				libtest.Assert(t, "Additional.TTL", c.exp[x].Additional[y].TTL, add.TTL, true)
+				libtest.Assert(t, "Additional.RData()", c.exp[x].Additional[y].RData(), add.RData(), true)
+			}
+		}
+	}
+
 }
