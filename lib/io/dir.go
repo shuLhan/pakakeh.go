fe9117e7283e6dd91fb23acba4c380a5e4857042
Shulhan <ms@kilabit.info>
2018-09-12 23:00:47 +0700

    lib/io: add function remove empty directory recursively

---
+++ lib/io/dir.go
@@ -0,0 +1,36 @@
+// Copyright 2018, Shulhan <ms@kilabit.info>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package io
+
+import (
+	"os"
+	"path/filepath"
+)
+
+//
+// RmdirEmptyAll remove directory in path if it's empty until one of the
+// parent is not empty.
+//
+func RmdirEmptyAll(path string) error {
+	if len(path) == 0 {
+		return nil
+	}
+	fi, err := os.Stat(path)
+	if err != nil {
+		return RmdirEmptyAll(filepath.Dir(path))
+	}
+	if !fi.IsDir() {
+		return nil
+	}
+	if !IsDirEmpty(path) {
+		return nil
+	}
+	err = os.Remove(path)
+	if err != nil {
+		return err
+	}
+
+	return RmdirEmptyAll(filepath.Dir(path))
+}
--- lib/io/io_test.go
+++ lib/io/io_test.go
@@ -6,10 +6,24 @@ import (
 	"testing"
 )
 
+func cleanup() {
+	// Cleaning up TestRmdirEmptyAll
+	_ = os.Remove("testdata/file")
+	_ = os.RemoveAll("testdata/a")
+	_ = os.RemoveAll("testdata/dirempty")
+}
+
 func TestMain(m *testing.M) {
+	cleanup()
+
 	sdebug := os.Getenv("DEBUG")
 	if len(sdebug) > 0 {
 		_debug, _ = strconv.Atoi(sdebug)
 	}
-	os.Exit(m.Run())
+
+	s := m.Run()
+
+	cleanup()
+
+	os.Exit(s)
 }
