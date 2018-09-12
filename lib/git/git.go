7ce478ca7702ccb451152d081679f6d9d201e64a
Shulhan <ms@kilabit.info>
2018-09-12 23:36:49 +0700

    lib/git: new package that wrap common git commands and parse git config
    List of current git commands that are wrapped,
    
    - Setting HEAD to specific revision: "git checkout <revision>"
    - Cloning repository: "git clone <remoteURL>"
    - Fetch all changes from remote: "git fetch --all"
    - Get tag on revision: "git describe --tags --exact-match <revision>"
    - Get latest commit: "git rev-parse --short <ref>"
    - Get latest tag
    - Get latest version (combination of get latest commit and tag)
    - Log revisions: "git --no-pager log --oneline"

---
+++ lib/git/git.go
@@ -0,0 +1,364 @@
+// Copyright 2018, Shulhan <ms@kilabit.info>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package git provide a wrapper for git comman line interface.
+package git
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/shuLhan/share/lib/ini"
+)
+
+const (
+	_defRemoteName = "origin"
+	_defRef        = "origin/master"
+	_defBranch     = "master"
+)
+
+var (
+	_debug  int
+	_stdout = os.Stdout
+	_stderr = os.Stderr
+)
+
+//
+// CheckoutRevision will set the HEAD to specific revision on specific branch.
+// Any untracked files and directories will be removed before checking out
+// existing branch or creating new branch.
+// If ref is empty, it will use default reference "origin/master".
+// If branch is empty, it will use default branch "master".
+// If revision is empty, it will do nothing.
+//
+func CheckoutRevision(repoDir, ref, branch, revision string) error {
+	if len(revision) == 0 {
+		return nil
+	}
+
+	cmd := exec.Command("git")
+	cmd.Args = append(cmd.Args, "clean", "-qdff")
+	cmd.Dir = repoDir
+	cmd.Stdout = _stdout
+	cmd.Stderr = _stderr
+	if _debug >= 1 {
+		fmt.Printf("= CheckoutRevision %s %s\n", cmd.Dir, cmd.Args)
+	}
+	err := cmd.Run()
+	if err != nil {
+		err = fmt.Errorf("CheckoutRevision: %s", err)
+		return err
+	}
+
+	err = FetchAll(repoDir)
+	if err != nil {
+		err = fmt.Errorf("CheckoutRevision: %s", err)
+		return err
+	}
+
+	cmd = exec.Command("git")
+	cmd.Dir = repoDir
+	cmd.Stdout = _stdout
+	cmd.Stderr = _stderr
+
+	if len(ref) == 0 {
+		ref = _defRef
+	}
+	cmd.Args = append(cmd.Args, "checkout", "--quiet", "--track", ref)
+
+	if len(branch) == 0 {
+		branch = _defBranch
+	}
+	cmd.Args = append(cmd.Args, "-B", branch)
+
+	if _debug >= 1 {
+		fmt.Printf("= CheckoutRevision %s %s\n", cmd.Dir, cmd.Args)
+	}
+	err = cmd.Run()
+	if err != nil {
+		err = fmt.Errorf("CheckoutRevision: %s", err)
+		return err
+	}
+
+	cmd = exec.Command("git")
+	cmd.Dir = repoDir
+	cmd.Stdout = _stdout
+	cmd.Stderr = _stderr
+
+	cmd.Args = append(cmd.Args, "reset", "--quiet", "--hard", revision)
+	if _debug >= 1 {
+		fmt.Printf("= CheckoutRevision %s %s\n", cmd.Dir, cmd.Args)
+	}
+	err = cmd.Run()
+	if err != nil {
+		err = fmt.Errorf("CheckoutRevision: %s", err)
+	}
+
+	return err
+}
+
+//
+// Clone the repository into destination directory.
+//
+// If destination directory is not empty it will return an error.
+//
+func Clone(remoteURL, dest string) (err error) {
+	err = os.MkdirAll(dest, 0700)
+	if err != nil {
+		err = fmt.Errorf("Clone: %s", err)
+		return
+	}
+
+	cmd := exec.Command("git")
+	cmd.Args = append(cmd.Args, "clone", "--quiet", remoteURL, ".")
+	cmd.Dir = dest
+	cmd.Stdout = _stdout
+	cmd.Stderr = _stderr
+
+	if _debug >= 1 {
+		fmt.Printf("= Clone %s %s %s\n", remoteURL, cmd.Dir, cmd.Args)
+	}
+
+	err = cmd.Run()
+	if err != nil {
+		err = fmt.Errorf("Clone: %s", err)
+	}
+
+	return
+}
+
+//
+// FetchAll will fetch the latest commits from remote.
+//
+func FetchAll(repoDir string) error {
+	cmd := exec.Command("git")
+	cmd.Args = append(cmd.Args, "fetch", "--quiet", "--all")
+	cmd.Dir = repoDir
+	cmd.Stdout = _stdout
+	cmd.Stderr = _stderr
+
+	if _debug >= 1 {
+		fmt.Printf("= FetchAll %s %s\n", cmd.Dir, cmd.Args)
+	}
+
+	err := cmd.Run()
+	if err != nil {
+		err = fmt.Errorf("FetchAll: %s", err)
+	}
+
+	return err
+}
+
+//
+// GetRemoteURL return remote URL or error if repository is not git or url is
+// empty.
+// If remoteName is empty, it will be set to default ("origin").
+//
+func GetRemoteURL(repoDir, remoteName string) (url string, err error) {
+	if len(remoteName) == 0 {
+		remoteName = _defRemoteName
+	}
+
+	gitConfig := filepath.Join(repoDir, ".git", "config")
+
+	gitIni, err := ini.Open(gitConfig)
+	if err != nil {
+		err = fmt.Errorf("GetRemote: %s", err)
+		return
+	}
+
+	url, ok := gitIni.Get("remote", remoteName, "url")
+	if !ok {
+		err = fmt.Errorf("GetRemote: Empty or invalid remote name")
+	}
+
+	return
+}
+
+//
+// GetTag get the tag from revision.  If revision is empty it's default to
+// "HEAD".
+//
+func GetTag(repoDir, revision string) (tag string, err error) {
+	if len(revision) == 0 {
+		revision = "HEAD"
+	}
+
+	cmd := exec.Command("git")
+	cmd.Args = append(cmd.Args, "describe", "--tags", "--exact-match", revision)
+	cmd.Dir = repoDir
+	cmd.Stderr = _stderr
+
+	if _debug >= 1 {
+		fmt.Printf("GetTag %s %s\n", cmd.Dir, cmd.Args)
+	}
+
+	btag, err := cmd.Output()
+	if err != nil {
+		err = fmt.Errorf("GetTag: %s", err)
+		return
+	}
+
+	tag = string(bytes.TrimSpace(btag))
+
+	return
+}
+
+//
+// LatestCommit get the latest commit hash in short format from "ref".
+// If ref is empty, its default to "origin/master".
+//
+func LatestCommit(repoDir, ref string) (commit string, err error) {
+	if len(ref) == 0 {
+		ref = _defRef
+	}
+
+	cmd := exec.Command("git")
+	cmd.Args = append(cmd.Args, "rev-parse", "--short", ref)
+	cmd.Dir = repoDir
+	cmd.Stderr = _stderr
+
+	if _debug >= 1 {
+		fmt.Printf("LatestCommit %s %s\n", cmd.Dir, cmd.Args)
+	}
+
+	bcommit, err := cmd.Output()
+	if err != nil {
+		err = fmt.Errorf("LatestCommit: %s", err)
+		return
+	}
+
+	commit = string(bytes.TrimSpace(bcommit))
+
+	return
+}
+
+//
+// LatestTag get latest tag.
+//
+func LatestTag(repoDir string) (tag string, err error) {
+	cmd := exec.Command("git")
+	cmd.Args = append(cmd.Args, "rev-list", "--tags", "--max-count=1")
+	cmd.Dir = repoDir
+	cmd.Stderr = _stderr
+
+	if _debug >= 1 {
+		fmt.Printf("= LatestTag %s %s\n", cmd.Dir, cmd.Args)
+	}
+
+	bout, err := cmd.Output()
+	if err != nil {
+		err = fmt.Errorf("LatestTag: %s", err)
+		return
+	}
+
+	out := string(bytes.TrimSpace(bout))
+
+	cmd = exec.Command("git")
+	cmd.Args = append(cmd.Args, "describe", "--tags", "--abbrev=0", out)
+	cmd.Dir = repoDir
+	cmd.Stderr = _stderr
+
+	if _debug >= 1 {
+		fmt.Printf("= LatestTag %s %s\n", cmd.Dir, cmd.Args)
+	}
+
+	bout, err = cmd.Output()
+	if err != nil {
+		err = fmt.Errorf("LatestTag: %s", err)
+		return
+	}
+
+	tag = string(bytes.TrimSpace(bout))
+
+	return
+}
+
+//
+// LatestVersion will try to get latest tag from repository.
+// If it's fail get the latest commit hash.
+//
+func LatestVersion(repoDir string) (version string, err error) {
+	version, err = LatestTag(repoDir)
+	if err == nil {
+		return
+	}
+
+	version, err = LatestCommit(repoDir, "")
+	if err == nil {
+		return
+	}
+
+	err = fmt.Errorf("GetVersion: %s", err)
+	return
+}
+
+//
+// LogRevisions get commits between two revisions.
+//
+func LogRevisions(repoDir, prevRevision, nextRevision string) error {
+	cmd := exec.Command("git")
+	cmd.Args = append(cmd.Args, "--no-pager", "log", "--oneline",
+		prevRevision+"..."+nextRevision)
+	cmd.Dir = repoDir
+	cmd.Stdout = _stdout
+	cmd.Stderr = _stderr
+
+	if _debug >= 1 {
+		fmt.Printf("= CompareRevisions %s %s\n", cmd.Dir, cmd.Args)
+	}
+
+	err := cmd.Run()
+	if err != nil {
+		err = fmt.Errorf("CompareRevisions: %s", err)
+	}
+
+	return err
+}
+
+//
+// RemoteChange change current repository remote name (e.g. "origin") to new
+// remote name and URL.
+//
+func RemoteChange(repoDir, oldName, newName, newURL string) error {
+	if len(repoDir) == 0 {
+		return nil
+	}
+
+	cmd := exec.Command("git")
+	cmd.Args = append(cmd.Args, "remote", "remove", oldName)
+	cmd.Dir = repoDir
+	cmd.Stdout = _stdout
+	cmd.Stderr = _stderr
+
+	if _debug >= 1 {
+		fmt.Printf("= RemoteChange %s %s\n", cmd.Dir, cmd.Args)
+	}
+
+	err := cmd.Run()
+	if err != nil {
+		err = fmt.Errorf("RemoteChange: %s", err)
+		return err
+	}
+
+	cmd = exec.Command("git")
+	cmd.Args = append(cmd.Args, "remote", "add", newName, newURL)
+	cmd.Dir = repoDir
+	cmd.Stdout = _stdout
+	cmd.Stderr = _stderr
+
+	if _debug >= 1 {
+		fmt.Printf("= RemoteChange %s %s\n", cmd.Dir, cmd.Args)
+	}
+
+	err = cmd.Run()
+	if err != nil {
+		err = fmt.Errorf("RemoteChange: %s", err)
+	}
+
+	return err
+}
---
+++ lib/git/git_test.go
@@ -0,0 +1,337 @@
+// Copyright 2018, Shulhan <ms@kilabit.info>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package git
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/shuLhan/share/lib/test"
+	"github.com/shuLhan/share/lib/test/mock"
+)
+
+const _testRepoDir = "testdata/beku_test"
+
+var (
+	_testRemoteURL string
+)
+
+func TestClone(t *testing.T) {
+	cases := []struct {
+		desc, dest, expErr, expStderr, expStdout string
+	}{{
+		desc:      "Clone on non empty directory",
+		dest:      "testdata/notempty",
+		expErr:    "Clone: exit status 128",
+		expStderr: "fatal: destination path '.' already exists and is not an empty directory.\n",
+	}, {
+		desc: "Clone on non existen directory",
+		dest: _testRepoDir,
+	}}
+
+	for _, c := range cases {
+		t.Log(c.desc)
+		mock.Reset(true)
+
+		err := Clone(_testRemoteURL, c.dest)
+		if err != nil {
+			test.Assert(t, "err", c.expErr, err.Error(), true)
+		}
+
+		test.Assert(t, "stderr", c.expStderr, mock.Error(), true)
+		test.Assert(t, "stdout", c.expStdout, mock.Output(), true)
+	}
+}
+
+func TestCheckoutRevision(t *testing.T) {
+	cases := []struct {
+		desc      string
+		remote    string
+		branch    string
+		revision  string
+		expErr    string
+		expStderr string
+		expStdout string
+	}{{
+		desc: "With empty revision",
+	}, {
+		desc:     "With unknown revision",
+		branch:   "beku",
+		revision: "xxxyyyzzz",
+		expErr:   "CheckoutRevision: exit status 128",
+		expStderr: `fatal: ambiguous argument 'xxxyyyzzz': unknown revision or path not in the working tree.
+Use '--' to separate paths from revisions, like this:
+'git <command> [<revision>...] -- [<file>...]'
+`,
+	}, {
+		desc:     "With valid revision",
+		branch:   "beku",
+		revision: "d6ad9da",
+	}}
+
+	for _, c := range cases {
+		t.Log(c.desc)
+		mock.Reset(true)
+
+		err := CheckoutRevision(_testRepoDir, c.remote, c.branch, c.revision)
+		if err != nil {
+			test.Assert(t, "err", c.expErr, err.Error(), true)
+		}
+
+		test.Assert(t, "stderr", c.expStderr, mock.Error(), true)
+		test.Assert(t, "stdout", c.expStdout, mock.Output(), true)
+	}
+}
+
+func TestGetRemoteURL(t *testing.T) {
+	cases := []struct {
+		desc                 string
+		remoteName           string
+		expErr               string
+		expStderr, expStdout string
+		exp                  string
+	}{{
+		desc:       "With invalid remote name",
+		remoteName: "upstream",
+		expErr:     "GetRemote: Empty or invalid remote name",
+	}, {
+		desc: "With empty remote name",
+		exp:  _testRemoteURL,
+	}}
+
+	for _, c := range cases {
+		t.Log(c.desc)
+		mock.Reset(true)
+
+		got, err := GetRemoteURL(_testRepoDir, c.remoteName)
+		if err != nil {
+			test.Assert(t, "err", c.expErr, err.Error(), true)
+		}
+
+		test.Assert(t, "url", c.exp, got, true)
+		test.Assert(t, "stderr", c.expStderr, mock.Error(), true)
+		test.Assert(t, "stdout", c.expStdout, mock.Output(), true)
+	}
+
+}
+
+func TestGetTag(t *testing.T) {
+	cases := []struct {
+		desc      string
+		revision  string
+		expErr    string
+		expStderr string
+		expStdout string
+	}{{
+		desc:      "With current HEAD",
+		expErr:    "GetTag: exit status 128",
+		expStderr: "fatal: no tag exactly matches 'd6ad9dabc61f72558013bb05e91bf273c491e39c'\n",
+	}, {
+		desc:      "With revision",
+		revision:  "ec65455",
+		expStdout: "v0.1.0",
+	}, {
+		desc:      "With revision",
+		revision:  "582b912",
+		expStdout: "v0.2.0",
+	}}
+
+	for _, c := range cases {
+		t.Log(c.desc)
+		mock.Reset(true)
+
+		got, err := GetTag(_testRepoDir, c.revision)
+		if err != nil {
+			test.Assert(t, "err", c.expErr, err.Error(), true)
+		}
+
+		test.Assert(t, "stderr", c.expStderr, mock.Error(), true)
+		test.Assert(t, "stdout", c.expStdout, got, true)
+	}
+}
+
+func TestLatestCommit(t *testing.T) {
+	cases := []struct {
+		desc      string
+		ref       string
+		expErr    string
+		expStderr string
+		expStdout string
+	}{{
+		desc:      "With invalid ref",
+		ref:       "upstream/master",
+		expErr:    "LatestCommit: exit status 128",
+		expStderr: "fatal: Needed a single revision\n",
+	}, {
+		desc:      "With empty ref",
+		expStdout: "c9f69fb",
+	}}
+
+	for _, c := range cases {
+		t.Log(c.desc)
+		mock.Reset(true)
+
+		got, err := LatestCommit(_testRepoDir, c.ref)
+		if err != nil {
+			test.Assert(t, "err", c.expErr, err.Error(), true)
+		}
+
+		test.Assert(t, "stderr", c.expStderr, mock.Error(), true)
+		test.Assert(t, "stdout", c.expStdout, got, true)
+	}
+}
+
+func TestLatestTag(t *testing.T) {
+	cases := []struct {
+		desc      string
+		expErr    string
+		expStderr string
+		expStdout string
+	}{{
+		desc:      "With default ref",
+		expStdout: "v0.2.0",
+	}}
+
+	for _, c := range cases {
+		t.Log(c.desc)
+		mock.Reset(true)
+
+		got, err := LatestTag(_testRepoDir)
+		if err != nil {
+			test.Assert(t, "err", c.expErr, err.Error(), true)
+		}
+
+		test.Assert(t, "stderr", c.expStderr, mock.Error(), true)
+		test.Assert(t, "stdout", c.expStdout, got, true)
+	}
+}
+
+func TestLatestVersion(t *testing.T) {
+	cases := []struct {
+		desc      string
+		expErr    string
+		expStderr string
+		expStdout string
+		exp       string
+	}{{
+		desc: "With default repo",
+		exp:  "v0.2.0",
+	}}
+
+	for _, c := range cases {
+		t.Log(c.desc)
+		mock.Reset(true)
+
+		got, err := LatestVersion(_testRepoDir)
+		if err != nil {
+			test.Assert(t, "err", c.expErr, err.Error(), true)
+		}
+
+		test.Assert(t, "version", c.exp, got, true)
+		test.Assert(t, "stderr", c.expStderr, mock.Error(), true)
+		test.Assert(t, "stdout", c.expStdout, mock.Output(), true)
+	}
+}
+
+func TestLogRevisions(t *testing.T) {
+	cases := []struct {
+		desc         string
+		prevRevision string
+		nextRevision string
+		expErr       string
+		expStderr    string
+		expStdout    string
+	}{{
+		desc: "With both revision are empty",
+	}, {
+		desc:         "With previous revision is empty",
+		nextRevision: "582b912",
+		expStdout: `582b912 Add feature B.
+ec65455 Add feature A.
+`,
+	}, {
+		desc:         "With next revision is empty",
+		prevRevision: "582b912",
+		expStdout: `582b912 Add feature B.
+ec65455 Add feature A.
+`,
+	}, {
+		desc:         "With previous and next revisions",
+		prevRevision: "582b912",
+		nextRevision: "c9f69fb",
+		expStdout: `c9f69fb Rename test to main.go
+`,
+	}}
+
+	for _, c := range cases {
+		t.Log(c.desc)
+		mock.Reset(true)
+
+		err := LogRevisions(_testRepoDir, c.prevRevision, c.nextRevision)
+		if err != nil {
+			test.Assert(t, "err", c.expErr, err.Error(), true)
+		}
+
+		test.Assert(t, "stderr", c.expStderr, mock.Error(), true)
+		test.Assert(t, "stdout", c.expStdout, mock.Output(), true)
+	}
+}
+
+func TestRemoteChange(t *testing.T) {
+	cases := []struct {
+		desc                     string
+		oldName, newName, newURL string
+		expErr                   string
+		expStderr                string
+		expStdout                string
+	}{{
+		desc:      "With empty oldName",
+		expErr:    "RemoteChange: exit status 128",
+		expStderr: "fatal: No such remote: ''\n",
+	}, {
+		desc:      "With empty newName",
+		oldName:   "origin",
+		expErr:    "RemoteChange: exit status 128",
+		expStderr: "fatal: '' is not a valid remote name\n",
+	}}
+
+	for _, c := range cases {
+		t.Log(c.desc)
+		mock.Reset(true)
+
+		err := RemoteChange(_testRepoDir, c.oldName, c.newName, c.newURL)
+		if err != nil {
+			test.Assert(t, "err", c.expErr, err.Error(), true)
+		}
+
+		test.Assert(t, "stderr", c.expStderr, mock.Error(), true)
+		test.Assert(t, "stdout", c.expStdout, mock.Output(), true)
+	}
+}
+
+func TestMain(m *testing.M) {
+	wd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	_testRemoteURL = wd + "/testdata/beku_test.git"
+
+	_stdout = mock.Stdout()
+	_stderr = mock.Stderr()
+
+	fmt.Printf("stdout: %+v\n", _stdout.Name())
+	fmt.Printf("stderr: %+v\n", _stderr.Name())
+
+	// Cleaning up for TestClone
+	os.RemoveAll(_testRepoDir)
+
+	s := m.Run()
+
+	mock.Close()
+
+	os.Exit(s)
+}
