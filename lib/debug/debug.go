be746238c08dc36991657c987d9d1d2bef9c954a
Shulhan <ms@kilabit.info>
2018-09-14 00:42:28 +0700

    lib/debug: new package for debugging
    The debug package provide global Value that is initialized from
    environment variable "DEBUG".
    
    User only need to include the package, and all initialization will be
    handled by init().

---
+++ lib/debug/debug.go
@@ -0,0 +1,33 @@
+// Copyright 2018, Shulhan <ms@kilabit.info>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package debug provide global debug variable, initialized through
+// environment variable "DEBUG" or directly.
+package debug
+
+import (
+	"os"
+	"strconv"
+	"sync"
+)
+
+var (
+	// Value contains DEBUG value from environment.
+	Value = 0
+	once  sync.Once
+)
+
+func loadEnvironment() {
+	v := os.Getenv("DEBUG")
+	if len(v) > 0 {
+		Value, _ = strconv.Atoi(v)
+	}
+}
+
+//
+// init initialize debug from system environment.
+//
+func init() {
+	once.Do(loadEnvironment)
+}
