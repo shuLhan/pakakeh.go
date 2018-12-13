26ed8311db8cc8b692754cfb0a3588172edf4d0b
Shulhan <ms@kilabit.info>
2018-12-13 22:09:22 +0700

    doc: add documentation for SMTP and DSN

--- lib/smtp/doc.go
+++ lib/smtp/doc.go
@@ -2,5 +2,5 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package smtp provides SMTP server and client implementations.
+// Package smtp provide a library for building SMTP server and client.
 package smtp
