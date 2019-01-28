dd287298d20f5f757a8714dd4f6afdfb2c1327b4
Shulhan <ms@kilabit.info>
2019-01-29 02:58:08 +0700

    lib/debug: add type to store relative and difference on memory heap
    This type, MemHeap, is simplified value of runtime.MemStats.
    The MemHeap represent the growth of heap memory in bytes and in number
    of allocated objects.

---
+++ lib/debug/memheap.go
@@ -0,0 +1,61 @@
+// Copyright 2019, Shulhan <ms@kilabit.info>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package debug
+
+import (
+	"runtime"
+)
+
+//
+// MemHeap store the difference between heap allocation.
+//
+type MemHeap struct {
+	// RelHeapAlloc difference between heap allocation, relative to the
+	// first time this object initialized.
+	RelHeapAlloc int64
+	// RelHeapObjects difference between heap objects, relative to the
+	// first time this object initialized.
+	RelHeapObjects int64
+
+	// DiffHeapObject different between last heap objects and with current
+	// relative heap objects.
+	// This value is equal to MemStats.Mallocs - MemStats.Frees.
+	// If its positive its means the number of objects allocated,
+	// otherwise its represent the number of objects freed.
+	DiffHeapObjects int64
+
+	memStats         runtime.MemStats
+	firstHeapAlloc   int64
+	firstHeapObjects int64
+	lastHeapObjects  int64
+}
+
+//
+// NewMemHeap create and initialize MemStatsDiff for the first time.
+//
+func NewMemHeap() (memHeap *MemHeap) {
+	firstStat := runtime.MemStats{}
+	runtime.ReadMemStats(&firstStat)
+
+	memHeap = &MemHeap{
+		firstHeapAlloc:   int64(firstStat.HeapAlloc),
+		firstHeapObjects: int64(firstStat.HeapObjects),
+	}
+
+	return
+}
+
+//
+// Collect and compute the difference on the current heap allocation (in
+// bytes) and heap objects.
+//
+func (msd *MemHeap) Collect() {
+	runtime.ReadMemStats(&msd.memStats)
+
+	msd.lastHeapObjects = msd.RelHeapObjects
+	msd.RelHeapAlloc = int64(msd.memStats.HeapAlloc) - msd.firstHeapAlloc
+	msd.RelHeapObjects = int64(msd.memStats.HeapObjects) - msd.firstHeapObjects
+	msd.DiffHeapObjects = msd.RelHeapObjects - msd.lastHeapObjects
+}
