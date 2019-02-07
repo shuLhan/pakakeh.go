6963ff35a5b22907f0cc0305561f6f3cd40bb99d
Shulhan <ms@kilabit.info>
2019-02-07 14:04:25 +0700

    dns: add pool for UDP client

---
+++ lib/dns/udpclientpool.go
@@ -0,0 +1,92 @@
+// Copyright 2019, Shulhan <ms@kilabit.info>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package dns
+
+import (
+	"errors"
+	"log"
+	"sync"
+)
+
+//
+// UDPClientPool contains a pool of UDP client connections.
+//
+// Any implementation that access UDPClient in multiple Go routines should
+// create one client per Go routine; instead of using a single UDP
+// client.
+// The reason for this is because UDP packet is asynchronous.
+//
+// WARNING: using pooling is only works if client only call Lookup or Query.
+// If implementation call Send() n client connection, make sure, it also call
+// Recv on the same routine before putting the client back to pool.
+//
+type UDPClientPool struct {
+	pool *sync.Pool
+	ns   []string
+	seq  int // sequence for the next name server.
+}
+
+//
+// NewUDPClientPool create pool for UDP connection using list of name servers.
+// If no name servers is defined it will return nil.
+//
+func NewUDPClientPool(nameServers []string) (ucp *UDPClientPool, err error) {
+	if len(nameServers) == 0 {
+		err = errors.New("udp: UDPClientPool: no name servers defined")
+		return nil, err
+	}
+	ucp = &UDPClientPool{
+		ns: nameServers,
+	}
+	ucp.pool = &sync.Pool{
+		New: ucp.newClient,
+	}
+
+	// Create new client for each name server, and push it to pool.
+	// This is required to check if each name server is a valid IP
+	// address because we did not want the New method return nil client
+	// later.
+	for x := 0; x < len(nameServers); x++ {
+		cl, err := NewUDPClient(nameServers[x])
+		if err != nil {
+			return nil, err
+		}
+		ucp.Put(cl)
+	}
+
+	return ucp, nil
+}
+
+//
+// newClient create a new udp client.
+//
+func (ucp *UDPClientPool) newClient() interface{} {
+	ucp.seq %= len(ucp.ns)
+	cl, err := NewUDPClient(ucp.ns[ucp.seq])
+	if err != nil {
+		log.Fatal("udp: UDPClientPool: cannot create new client: ", err)
+	}
+	ucp.seq++
+	return cl
+}
+
+//
+// Get return UDP client.
+//
+func (ucp *UDPClientPool) Get() *UDPClient {
+	return ucp.pool.Get().(*UDPClient)
+}
+
+//
+// Put the UDP client into pool.
+//
+// WARNING: any client connection that call Send(), MUST call Recv()
+// before putting client back to pool.  You have been warned.
+//
+func (ucp *UDPClientPool) Put(cl interface{}) {
+	if cl != nil {
+		ucp.pool.Put(cl)
+	}
+}
---
+++ lib/dns/udpclientpool_test.go
@@ -0,0 +1,61 @@
+// Copyright 2019, Shulhan <ms@kilabit.info>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package dns
+
+import (
+	"sync"
+	"testing"
+
+	"github.com/shuLhan/share/lib/test"
+)
+
+func TestNewUDPClientPool(t *testing.T) {
+	cases := []struct {
+		desc   string
+		ns     []string
+		expErr string
+	}{{
+		desc:   "With empty name servers",
+		expErr: "udp: UDPClientPool: no name servers defined",
+	}, {
+		desc: "With one invalid name server",
+		ns: []string{
+			testServerAddress,
+			"notipaddress",
+		},
+		expErr: "invalid host address",
+	}, {
+		desc: "With single name server",
+		ns: []string{
+			testServerAddress,
+		},
+	}}
+
+	for _, c := range cases {
+		t.Log(c.desc)
+
+		ucp, err := NewUDPClientPool(c.ns)
+		if err != nil {
+			test.Assert(t, "error", c.expErr, err.Error(), true)
+			continue
+		}
+
+		var wg sync.WaitGroup
+		qname := []byte("kilabit.info")
+		for x := 0; x < 10; x++ {
+			wg.Add(1)
+			go func() {
+				cl := ucp.Get()
+				msg, err := cl.Lookup(QueryTypeA, QueryClassIN, qname)
+				if err != nil {
+					t.Log("Lookup error: ", err.Error())
+				}
+				t.Logf("Lookup response: %+v\n", msg.Header)
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+	}
+}
