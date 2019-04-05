f5818b4362ca4ddd1e30313c31a2fc3972649673
Shulhan <ms@kilabit.info>
2019-04-05 17:50:57 +0700

    crypto: new package that provide a wrapper for standard crypto
    This package provide a function to load private key from file, which is
    not available in standard crypto package.

---
+++ lib/crypto/crypto.go
@@ -0,0 +1,35 @@
+// Copyright 2019, Shulhan <ms@kilabit.info>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//
+// Package crypto provide a wrapper for standard crypto package.
+//
+package crypto
+
+import (
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"io/ioutil"
+)
+
+//
+// LoadPrivateKey read and parse PEM formatted private key from file.
+//
+func LoadPrivateKey(file string) (pkey *rsa.PrivateKey, err error) {
+	rawPEM, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+
+	block, _ := pem.Decode(rawPEM)
+	if block == nil {
+		return nil, fmt.Errorf("crypto: failed to parse PEM block from %q", file)
+	}
+
+	pkey, err = x509.ParsePKCS1PrivateKey(block.Bytes)
+
+	return
+}
