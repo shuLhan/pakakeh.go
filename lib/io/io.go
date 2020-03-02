c926bdf03a4da1541af5edcd1fb055c48c5ce3d2
Shulhan <m.shulhan@gmail.com>
2020-03-02 23:09:49 +0700

    io: add function to copy file

--- lib/io/io.go
+++ lib/io/io.go
@@ -7,3 +7,61 @@
 // from standard input.
 //
 package io
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+//
+// Copy file from in to out.
+// If the output file is already exist, it will be truncated.
+// If the file is not exist, it will created with permission set to user's
+// read-write only.
+//
+func Copy(out, in string) (err error) {
+	fin, err := os.Open(in)
+	if err != nil {
+		return fmt.Errorf("Copy: failed to open input file: %s", err)
+	}
+
+	fout, err := os.OpenFile(out, os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		return fmt.Errorf("Copy: failed to open output file: %s", err)
+	}
+
+	defer func() {
+		err := fout.Close()
+		if err != nil {
+			log.Printf("Copy: failed to close output file: %s", err)
+		}
+	}()
+	defer func() {
+		err := fin.Close()
+		if err != nil {
+			log.Printf("Copy: failed to close input file: %s", err)
+		}
+	}()
+
+	buf := make([]byte, 1024)
+	for {
+		n, err := fin.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return err
+		}
+		if n == 0 {
+			break
+		}
+		_, err = fout.Write(buf[:n])
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
