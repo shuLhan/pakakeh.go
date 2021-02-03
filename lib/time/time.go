a4acb2da36563fe814ad8ccf325cd733a9e2e1f1
Shulhan <m.shulhan@gmail.com>
2021-02-03 13:04:47 +0700

    time: add function UnixMicro that return UNIX time in micro seconds
    While at it, replace the unit tests with an example so we can both do
    unit testing and provide an examples in documentation.

--- lib/time/time.go
+++ lib/time/time.go
@@ -37,10 +37,23 @@ var (
 
 //
 // Microsecond return the microsecond value of time.
+// For example, if the unix nano seconds is 1612331218913557000 then the micro
+// second value is 913557.
+//
+// To get the unix microsecond use UnixMicro().
 //
 func Microsecond(t *time.Time) int64 {
 	seconds := t.Unix() * int64(time.Second)
 	return (t.UnixNano() - seconds) / int64(time.Microsecond)
+}
+
+//
+// UnixMicro returns t as a Unix time in microsecond.
+// For example, if the unix nano seconds is 1612331218913557000 then the
+// UnixMicro value is 1612331218913557.
+//
+func UnixMicro(t *time.Time) int64 {
+	return t.UnixNano() / int64(time.Microsecond)
 }
 
 //
---
+++ lib/time/time_example_test.go
@@ -0,0 +1,38 @@
+// Copyright 2021, Shulhan <ms@kilabit.info>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package time
+
+import (
+	"fmt"
+	"time"
+)
+
+func ExampleMicrosecond() {
+	nano := time.Unix(1612331000, 123456789)
+	fmt.Printf("%d", Microsecond(&nano))
+	//Output:
+	//123456
+}
+
+func ExampleUnixMicro() {
+	nano := time.Unix(1612331000, 123456789)
+	fmt.Printf("%d", UnixMicro(&nano))
+	//Output:
+	//1612331000123456
+}
+
+func ExampleUnixMilli() {
+	nano := time.Unix(1612331000, 123456789)
+	fmt.Printf("%d", UnixMilli(nano))
+	//Output:
+	//1612331000123
+}
+
+func ExampleUnixMilliString() {
+	nano := time.Unix(1612331000, 123456789)
+	fmt.Printf("%s", UnixMilliString(nano))
+	//Output:
+	//1612331000123
+}
