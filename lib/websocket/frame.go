b3220083906406634edcd83875e8816d6c04c089
Shulhan <ms@kilabit.info>
2022-06-08 23:38:33 +0700

    lib/websocket: realign struct Frame
    This realign make struct Frame size from 72 to 56 bytes (-16 bytes).

--- lib/websocket/frame.go
+++ lib/websocket/frame.go
@@ -12,52 +12,8 @@ import (
 
 // Frame represent a WebSocket data protocol.
 type Frame struct {
-	// fin Indicates that this is the final fragment in a message.
-	// The first fragment MAY also be the final fragment.
-	fin byte
-
-	// rsv1, rsv2, and rsv3 is reserved bits in frame.
-	rsv1 byte
-	rsv2 byte
-	rsv3 byte
-
-	// opcode (4 bits) defines the interpretation of the "Payload data".
-	// If an unknown opcode is received, the receiving endpoint MUST _Fail
-	// the WebSocket Connection_.  The following values are defined.
-	opcode Opcode
-
 	//
-	// masked (1 bit) defines whether the "Payload data" is masked.
-	// If set to 1, a masking key is present in masking-key, and this is
-	// used to unmask the "Payload data" as per Section 5.3.  All frames
-	// sent from client to server have this bit set to 1.
-	//
-	masked byte
-
-	// closeCode represent the status of control frame close request.
-	closeCode CloseCode
-
-	//
-	// len represent Payload length:  7 bits, 7+16 bits, or 7+64 bits
-	//
-	// The length of the "Payload data", in bytes: if 0-125, that is the
-	// payload length.  If 126, the following 2 bytes interpreted as a
-	// 16-bit unsigned integer are the payload length.  If 127, the
-	// following 8 bytes interpreted as a 64-bit unsigned integer (the
-	// most significant bit MUST be 0) are the payload length.  Multibyte
-	// length quantities are expressed in network byte order.  Note that
-	// in all cases, the minimal number of bytes MUST be used to encode
-	// the length, for example, the length of a 124-byte-long string
-	// can't be encoded as the sequence 126, 0, 124.  The payload length
-	// is the length of the "Extension data" + the length of the
-	// "Application data".  The length of the "Extension data" may be
-	// zero, in which case the payload length is the length of the
-	// "Application data".
-	//
-	len uint64
-
-	//
-	// maskKey:  0 or 4 bytes
+	// maskKey size is 0 or 4 bytes.
 	//
 	// All frames sent from the client to the server are masked by a
 	// 32-bit value that is contained within the frame.  This field is
@@ -96,6 +52,50 @@ type Frame struct {
 	// payload.
 	//
 	chopped []byte
+
+	//
+	// len represent Payload length:  7 bits, 7+16 bits, or 7+64 bits
+	//
+	// The length of the "Payload data", in bytes: if 0-125, that is the
+	// payload length.  If 126, the following 2 bytes interpreted as a
+	// 16-bit unsigned integer are the payload length.  If 127, the
+	// following 8 bytes interpreted as a 64-bit unsigned integer (the
+	// most significant bit MUST be 0) are the payload length.  Multibyte
+	// length quantities are expressed in network byte order.  Note that
+	// in all cases, the minimal number of bytes MUST be used to encode
+	// the length, for example, the length of a 124-byte-long string
+	// can't be encoded as the sequence 126, 0, 124.  The payload length
+	// is the length of the "Extension data" + the length of the
+	// "Application data".  The length of the "Extension data" may be
+	// zero, in which case the payload length is the length of the
+	// "Application data".
+	//
+	len uint64
+
+	// closeCode represent the status of control frame close request.
+	closeCode CloseCode
+
+	// opcode (4 bits) defines the interpretation of the "Payload data".
+	// If an unknown opcode is received, the receiving endpoint MUST _Fail
+	// the WebSocket Connection_.  The following values are defined.
+	opcode Opcode
+
+	//
+	// masked (1 bit) defines whether the "Payload data" is masked.
+	// If set to 1, a masking key is present in masking-key, and this is
+	// used to unmask the "Payload data" as per Section 5.3.  All frames
+	// sent from client to server have this bit set to 1.
+	//
+	masked byte
+
+	// fin Indicates that this is the final fragment in a message.
+	// The first fragment MAY also be the final fragment.
+	fin byte
+
+	// rsv1, rsv2, and rsv3 is reserved bits in frame.
+	rsv1 byte
+	rsv2 byte
+	rsv3 byte
 
 	// isComplete will be true if all frame's field completely filled.
 	isComplete bool
