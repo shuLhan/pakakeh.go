c80513b67f83f40f243c46c500d67a81f5b96df2
Shulhan <ms@kilabit.info>
2022-07-23 16:33:14 +0700

    lib/ini: convert the Get test using test.Data

--- lib/ini/ini_test.go
+++ lib/ini/ini_test.go
@@ -6,11 +6,9 @@ package ini
 
 import (
 	"bytes"
-	"fmt"
 	"testing"
 	"time"
 
-	"github.com/shuLhan/share/lib/debug"
 	"github.com/shuLhan/share/lib/test"
 )
 
@@ -234,336 +232,53 @@ func TestAddSection(t *testing.T) {
 	}
 }
 
-func TestGet(t *testing.T) {
-	var (
-		err error
-		got string
-		ok  bool
-	)
-
-	inputIni, err := Open(testdataInputIni)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	cases := []struct {
-		desc   string
-		sec    string
-		sub    string
-		key    string
-		expVal string
-		expOk  bool
-	}{{
-		desc: `With empty section`,
-		sub:  "devel",
-		key:  "remote",
-	}, {
-		desc:   `With empty subsection`,
-		sec:    "user",
-		key:    "name",
-		expVal: "Shulhan",
-		expOk:  true,
-	}, {
-		desc: `With empty key`,
-		sec:  "user",
-	}, {
-		desc: `With invalid section`,
-		sec:  "sectionnotexist",
-		key:  "name",
-	}, {
-		desc: `With invalid subsection`,
-		sec:  "branch",
-		sub:  "notexist",
-		key:  "remote",
-	}, {
-		desc: `With invalid key`,
-		sec:  "branch",
-		sub:  "devel",
-		key:  "user",
-	}, {
-		desc: `With empty value`,
-		sec:  "http",
-		key:  "sslVerify",
-	}}
-
-	for _, c := range cases {
-		t.Logf("%+v", c)
-
-		got, ok = inputIni.Get(c.sec, c.sub, c.key, "")
-		if !ok {
-			test.Assert(t, "ok", c.expOk, ok)
-			continue
-		}
-
-		test.Assert(t, "value", c.expVal, got)
-	}
-}
-
-func TestGetInputIni(t *testing.T) {
-	inputIni, err := Open(testdataInputIni)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	cases := []struct {
-		sec     string
-		sub     string
-		keys    []string
-		expVals []string
-	}{{
-		sec: "core",
-		keys: []string{
-			"filemode",
-			"gitProxy",
-			"pager",
-			"editor",
-			"autocrlf",
-		},
-		expVals: []string{
-			"true",
-			"default-proxy",
-			"less -R",
-			"nvim",
-			"false",
-		},
-	}, {
-		sec: "diff",
-		keys: []string{
-			"external",
-			"renames",
-		},
-		expVals: []string{
-			"/usr/local/bin/diff-wrapper",
-			"true",
-		},
-	}, {
-		sec: "user",
-		keys: []string{
-			"name",
-			"email",
-		},
-		expVals: []string{
-			"Shulhan",
-			"ms@kilabit.info",
-		},
-	}, {
-		sec: "http",
-		keys: []string{
-			"sslVerify",
-			"cookiefile",
-		},
-		expVals: []string{
-			"",
-			"/home/ms/.gitcookies",
-		},
-	}, {
-		sec: "http",
-		sub: "https://weak.example.com",
-		keys: []string{
-			"sslVerify",
-			"cookiefile",
-		},
-		expVals: []string{
-			"false",
-			"/tmp/cookie.txt",
-		},
-	}, {
-		sec: "branch",
-		sub: "devel",
-		keys: []string{
-			"remote",
-			"merge",
-		},
-		expVals: []string{
-			"origin",
-			"refs/heads/devel",
-		},
-	}, {
-		sec: "include",
-		keys: []string{
-			"path",
-		},
-		expVals: []string{
-			"~/foo.inc",
-		},
-	}, {
-		sec: "includeIf",
-		sub: "gitdir:/path/to/foo/.git",
-		keys: []string{
-			"path",
-		},
-		expVals: []string{
-			"/path/to/foo.inc",
-		},
-	}, {
-		sec: "includeIf",
-		sub: "gitdir:/path/to/group/",
-		keys: []string{
-			"path",
-		},
-		expVals: []string{
-			"foo.inc",
-		},
-	}, {
-		sec: "includeIf",
-		sub: "gitdir:~/to/group/",
-		keys: []string{
-			"path",
-		},
-		expVals: []string{
-			"/path/to/foo.inc",
-		},
-	}, {
-		sec:     "color",
-		keys:    []string{"ui"},
-		expVals: []string{"true"},
-	}, {
-		sec: "gui",
-		keys: []string{
-			"fontui",
-			"fontdiff",
-			"diffcontext",
-			"spellingdictionary",
-		},
-		expVals: []string{
-			"-family \"xos4 Terminus\" -size 10 -weight normal -slant roman -underline 0 -overstrike 0",
-			"-family \"xos4 Terminus\" -size 10 -weight normal -slant roman -underline 0 -overstrike 0",
-			"4",
-			"none",
-		},
-	}, {
-		sec: "svn",
-		keys: []string{
-			"rmdir",
-		},
-		expVals: []string{
-			"true",
-		},
-	}, {
-		sec: "alias",
-		keys: []string{
-			"change",
-			"gofmt",
-			"mail",
-			"pending",
-			"submit",
-			"sync",
-			"tree",
-			"to-master",
-			"to-prod",
-		},
-		expVals: []string{
-			"codereview change",
-			"codereview gofmt",
-			"codereview mail",
-			"codereview pending",
-			"codereview submit",
-			"codereview sync",
-			`!git --no-pager log --graph 		--date=format:'%Y-%m-%d' 		--pretty=format:'%C(auto,dim)%ad %<(7,trunc) %an %Creset%m %h %s %Cgreen%d%Creset' 		--exclude=*/production 		--exclude=*/dev-* 		--all -n 20`,
-			`!git stash -u 		&& git fetch origin 		&& git rebase origin/master 		&& git stash pop 		&& git --no-pager log --graph --decorate --pretty=oneline 			--abbrev-commit origin/master~1..HEAD`,
-			`!git stash -u 		&& git fetch origin 		&& git rebase origin/production 		&& git stash pop 		&& git --no-pager log --graph --decorate --pretty=oneline 			--abbrev-commit origin/production~1..HEAD`,
-		},
-	}, {
-		sec: "url",
-		sub: "git@github.com:",
-		keys: []string{
-			"insteadOf",
-		},
-		expVals: []string{
-			"https://github.com/",
-		},
-	}, {
-		sec: "last",
-		keys: []string{
-			"valid0",
-			"valid1",
-			"valid2",
-			"valid3",
-			"valid4",
-		},
-		expVals: []string{
-			"",
-			"",
-			"",
-			"",
-			"",
-		},
-	}}
-
-	var (
-		got string
-		ok  bool
-	)
-
-	for _, c := range cases {
-		t.Log(c)
-
-		if debug.Value >= 3 {
-			t.Logf("Section header: [%s %s]", c.sec, c.sub)
-			t.Logf(">>> keys: %s", c.keys)
-			t.Logf(">>> expVals: %s", c.expVals)
-		}
-
-		for x, k := range c.keys {
-			t.Log("  Get:", k)
-
-			got, ok = inputIni.Get(c.sec, c.sub, k, "")
-			if !ok {
-				t.Logf("Get: %s > %s > %s", c.sec, c.sub, k)
-				test.Assert(t, "ok", true, ok)
-				t.FailNow()
-			}
-
-			test.Assert(t, "value", c.expVals[x], got)
-		}
-	}
-}
-
 func TestIni_Get(t *testing.T) {
 	var (
-		cfg   *Ini
-		tdata *test.Data
-		got   string
-		def   string
-		tags  []string
-		keys  [][]byte
-		exps  [][]byte
-		key   []byte
-		err   error
-		x     int
-		ok    bool
+		cfg       *Ini
+		listTData []*test.Data
+		tdata     *test.Data
+		got       string
+		def       string
+		tags      []string
+		keys      [][]byte
+		exps      [][]byte
+		key       []byte
+		err       error
+		x         int
 	)
 
-	tdata, err = test.LoadData("testdata/get.txt")
+	listTData, err = test.LoadDataDir("testdata/")
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	cfg, err = Parse(tdata.Input["default"])
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	keys = bytes.Split(tdata.Input["keys"], []byte("\n"))
-	exps = bytes.Split(tdata.Output["default"], []byte("\n"))
-
-	if len(keys) != len(exps) {
-		t.Fatalf("%s: input keys length %d does not match with output %d",
-			tdata.Name, len(keys), len(exps))
-	}
-
-	for x, key = range keys {
-		if len(key) == 0 {
-			test.Assert(t, "Get", string(exps[x]), "")
-			continue
+	for _, tdata = range listTData {
+		cfg, err = Parse(tdata.Input["default"])
+		if err != nil {
+			t.Fatal(err)
 		}
 
-		tags = parseTag(string(key))
-		def = tags[3]
+		keys = bytes.Split(tdata.Input["keys"], []byte("\n"))
+		exps = bytes.Split(tdata.Output["default"], []byte("\n"))
 
-		got, ok = cfg.Get(tags[0], tags[1], tags[2], def)
-		got = fmt.Sprintf("%t %s.", ok, got)
+		if len(keys) != len(exps) {
+			t.Fatalf("%s: input keys length %d does not match with output %d",
+				tdata.Name, len(keys), len(exps))
+		}
 
-		test.Assert(t, "Get", string(exps[x]), got)
+		for x, key = range keys {
+			if len(key) == 0 {
+				test.Assert(t, "Get", string(exps[x]), "")
+				continue
+			}
+
+			tags = parseTag(string(key))
+			def = tags[3]
+
+			got, _ = cfg.Get(tags[0], tags[1], tags[2], def)
+			got = got + "."
+
+			test.Assert(t, "Get", string(exps[x]), got)
+		}
 	}
 }
