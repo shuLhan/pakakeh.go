2874354aa137c46c26de0740495d02f02dcc5462
Shulhan <ms@kilabit.info>
2022-07-27 01:28:41 +0700

    cmd/epoch: add command "help" and "version"

--- cmd/epoch/main.go
+++ cmd/epoch/main.go
@@ -2,38 +2,9 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-/*
-Program epoch print the current time (Unix seconds, milliseconds,
-nanoseconds, local time, and UTC time) or the time based on the epoch on
-first parameter.
-Usage,
-
-	epoch <unix-seconds|unix-milliseconds|unix-nanoseconds>
-
-Without a parameter, it will print the current time.
-With single parameter, it will print the time based on that epoch.
-
-Example,
-
-	$ epoch
-
-	Unix seconds: 1639896843
-	  Unix milli: 1639896843382
-	  Unix micro: 1639896843382879
-	   Unix nano: 1639896843382879358
-	  Local time: 2021-12-19 13:54:03.382879358 +0700 WIB m=+0.000041439
-	    UTC time: 2021-12-19 06:54:03.382879358 +0000 UTC
-
-
-	$ epoch 1639800000
-
-	Unix seconds: 1639800000
-	  Unix milli: 1639800000000
-	  Unix micro: 1639800000000000
-	   Unix nano: 1639800000000000000
-	  Local time: 2021-12-18 11:00:00 +0700 WIB
-	    UTC time: 2021-12-18 04:00:00 +0000 UTC
-*/
+// Program epoch print the current date and time (Unix seconds, milliseconds,
+// nanoseconds, local time, and UTC time) or the date and time based on the
+// epoch on first parameter.
 package main
 
 import (
@@ -41,46 +12,115 @@ import (
 	"fmt"
 	"log"
 	"strconv"
+	"strings"
 	"time"
+
+	"github.com/shuLhan/share"
+)
+
+const usage = `= epoch
+
+epoch print the current date and time (Unix seconds, milliseconds,
+nanoseconds, local time, and UTC time) or the date and time based on the epoch
+on first parameter.
+
+== SYNOPSIS
+
+	epoch <"help" |"version"|unix-seconds|unix-milliseconds|unix-nanoseconds>
+
+== ARGUMENTS
+
+Without a parameter, it will print the current time.
+With single parameter, it will print the time based on that epoch.
+
+help
+	Print the current command usage (this message).
+
+version
+	Print the command version.
+
+
+== EXAMPLE
+
+Print the current date and time,
+
+	$ epoch
+	     Second: 1639896843
+	Millisecond: 1639896843382
+	Microsecond: 1639896843382879
+	 Nanosecond: 1639896843382879358
+	 Local time: 2021-12-19 13:54:03.382879358 +0700 WIB m=+0.000041439
+	   UTC time: 2021-12-19 06:54:03.382879358 +0000 UTC
+
+Print the date and time at epoch 1639800000,
+
+	$ epoch 1639800000
+	     Second: 1639800000
+	Millisecond: 1639800000000
+	Microsecond: 1639800000000000
+	 Nanosecond: 1639800000000000000
+	 Local time: 2021-12-18 11:00:00 +0700 WIB
+	   UTC time: 2021-12-18 04:00:00 +0000 UTC`
+
+const (
+	cmdHelp    = "help"
+	cmdVersion = "version"
 )
 
 func main() {
+	var (
+		t     time.Time
+		cmd   string
+		epoch int64
+		err   error
+	)
+
+	log.SetFlags(0)
+	log.SetPrefix("epoch: ")
 	flag.Parse()
 
-	ts := flag.Arg(0)
+	cmd = strings.ToLower(flag.Arg(0))
 
-	if len(ts) == 0 {
+	if len(cmd) == 0 {
 		echo(time.Now())
 		return
 	}
 
-	var t time.Time
+	switch cmd {
+	case cmdHelp:
+		fmt.Println(usage)
+		return
 
-	epoch, err := strconv.ParseInt(ts, 10, 64)
-	if err != nil {
-		log.Fatalf("invalid epoch %s: %s", ts, err)
+	case cmdVersion:
+		fmt.Println("epoch v" + share.Version)
+		return
+
+	default:
+		epoch, err = strconv.ParseInt(cmd, 10, 64)
+		if err != nil {
+			log.Fatalf("invalid epoch %s: %s", cmd, err)
+		}
+
+		switch len(cmd) {
+		case 10: // Epoch in seconds.
+			t = time.Unix(epoch, 0)
+		case 13:
+			t = time.UnixMilli(epoch)
+		case 16:
+			t = time.UnixMicro(epoch)
+		}
+
+		echo(t)
 	}
-
-	switch len(ts) {
-	case 10: // Epoch in seconds.
-		t = time.Unix(epoch, 0)
-	case 13:
-		t = time.UnixMilli(epoch)
-	case 16:
-		t = time.UnixMicro(epoch)
-	}
-
-	echo(t)
 }
 
 func echo(t time.Time) {
-	fmt.Printf(`
-Unix seconds: %d
-  Unix milli: %d
-  Unix micro: %d
-   Unix nano: %d
-  Local time: %s
-    UTC time: %s
+	fmt.Printf(`     Second: %d
+Millisecond: %d
+Microsecond: %d
+ Nanosecond: %d
+ Local time: %s
+   UTC time: %s
 `, t.Unix(), t.UnixMilli(), t.UnixMicro(), t.UnixNano(), t, t.UTC())
 
 }
