3fe38be4bd4ed2ec1adf3b2cb64f876d0819a8de
Shulhan <ms@kilabit.info>
2022-07-27 01:55:30 +0700

    cmd/epoch: add flag to parse time from RFC3339 and RFC1123 format
    The flag for RFC1123 comes with two options one with string timezone
    (-rfc1123) and one with numeric time zone (-rfc1123z).

--- cmd/epoch/main.go
+++ cmd/epoch/main.go
@@ -26,7 +26,9 @@ on first parameter.
 
 == SYNOPSIS
 
-	epoch <"help" |"version"|unix-seconds|unix-milliseconds|unix-nanoseconds>
+	epoch <"help" |"version">
+	epoch <unix-seconds|unix-milliseconds|unix-nanoseconds>
+	epoch <"-rfc3339" | "-rfc1123" | "-rfc1123z"> <time>
 
 == ARGUMENTS
 
@@ -39,6 +41,15 @@ help
 version
 	Print the command version.
 
+-rfc3339 <time>
+	Print the epoch based on the RFC3339 date and time format.
+
+-rfc1123 <time>
+	Print the epoch based on the RFC1123 date and time format.
+
+-rfc1123 <time>
+	Print the epoch based on the RFC1123 (with numeric timezone) date and
+	time format.
 
 == EXAMPLE
 
@@ -60,7 +71,38 @@ Print the date and time at epoch 1639800000,
 	Microsecond: 1639800000000000
 	 Nanosecond: 1639800000000000000
 	 Local time: 2021-12-18 11:00:00 +0700 WIB
-	   UTC time: 2021-12-18 04:00:00 +0000 UTC`
+	   UTC time: 2021-12-18 04:00:00 +0000 UTC
+
+Print the epoch, date and time from RFC3339 format,
+
+	$ epoch -rfc3339 "2021-12-18T11:00:00+07:00"
+	     Second: 1639800000
+	Millisecond: 1639800000000
+	Microsecond: 1639800000000000
+	 Nanosecond: 1639800000000000000
+	 Local time: 2021-12-18 11:00:00 +0700 WIB
+	   UTC time: 2021-12-18 04:00:00 +0000 UTC
+
+Print the epoch, date and time from RFC1123 time,
+
+	$ epoch -rfc1123 "Sat, 18 Dec 2021 11:00:00 WIB"
+	     Second: 1639800000
+	Millisecond: 1639800000000
+	Microsecond: 1639800000000000
+	 Nanosecond: 1639800000000000000
+	 Local time: 2021-12-18 11:00:00 +0700 WIB
+	   UTC time: 2021-12-18 04:00:00 +0000 UTC
+
+Print the epoch, date and time from RFC1123Z (with numeric time zone) time,
+
+	$ epoch -rfc1123z "Sat, 18 Dec 2021 11:00:00 +0700"
+	     Second: 1639800000
+	Millisecond: 1639800000000
+	Microsecond: 1639800000000000
+	 Nanosecond: 1639800000000000000
+	 Local time: 2021-12-18 11:00:00 +0700 WIB
+	   UTC time: 2021-12-18 04:00:00 +0000 UTC
+`
 
 const (
 	cmdHelp    = "help"
@@ -69,15 +111,48 @@ const (
 
 func main() {
 	var (
-		t     time.Time
-		cmd   string
-		epoch int64
-		err   error
+		t         time.Time
+		cmd       string
+		flag3339  string
+		flag1123  string
+		flag1123z string
+		epoch     int64
+		err       error
 	)
 
 	log.SetFlags(0)
 	log.SetPrefix("epoch: ")
+
+	flag.StringVar(&flag3339, "rfc3339", "", "Print the epoch based on RFC3339 time format")
+	flag.StringVar(&flag1123, "rfc1123", "", "Print the epoch based on RFC1123 time format")
+	flag.StringVar(&flag1123z, "rfc1123z", "", "Print the epoch based on RFC1123Z time format")
+
 	flag.Parse()
+
+	if len(flag3339) > 0 {
+		t, err = time.Parse(time.RFC3339, flag3339)
+		if err != nil {
+			log.Fatalf("invalid RFC3339 time: %s: %s", flag3339, err)
+		}
+		echo(t)
+		return
+	}
+	if len(flag1123) > 0 {
+		t, err = time.Parse(time.RFC1123, flag1123)
+		if err != nil {
+			log.Fatalf("invalid RFC1123 time: %s: %s", flag1123, err)
+		}
+		echo(t)
+		return
+	}
+	if len(flag1123z) > 0 {
+		t, err = time.Parse(time.RFC1123Z, flag1123z)
+		if err != nil {
+			log.Fatalf("invalid RFC1123Z time: %s: %s", flag1123z, err)
+		}
+		echo(t)
+		return
+	}
 
 	cmd = strings.ToLower(flag.Arg(0))
 
