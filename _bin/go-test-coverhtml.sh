cf3761a7ce3270bf44ad405cc10378ea49bd1d80
Shulhan <ms@kilabit.info>
2022-07-27 03:30:04 +0700

    _bin: add script to run Go test and generate HTML coverage
    The script accept one single argument: the path to package to
    be tested.
    If its empty default to current directory and sub-directories.

---
+++ _bin/go-test-coverhtml.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+## Script to run go test and generate HTML coverage.
+##
+## Argument: path to package or default to current package and its
+## subdirectory.
+
+PKGS=${1:-./...}
+
+CGO_ENABLED=1
+go test -race -p=1 -coverprofile=cover.out ${PKGS} && \
+	go tool cover -html=cover.out -o cover.html
