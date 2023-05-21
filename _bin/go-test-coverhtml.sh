8ae5db78ec8c2626cf454af55d605310809c8a1a
Shulhan <ms@kilabit.info>
2023-05-21 13:22:18 +0700

    _bin: add parameter to specific test in go-test-coverhtml
    The second parameter is optional.
    It is passed to -run= argument in "go test".
    Default value is ".", or all functions.

--- _bin/go-test-coverhtml.sh
+++ _bin/go-test-coverhtml.sh
@@ -1,12 +1,25 @@
 #!/bin/sh
 
-## Script to run go test and generate HTML coverage.
-##
-## Argument: path to package or default to current package and its
-## subdirectory.
+usage() {
+cat << EOF
+Script to run Go test and generate HTML coverage.
 
-PKGS=${1:-./...}
+Arguments,
+1: optional, path to package. Default "./...".
+2: optional, function name to be tested. Default to all functions.
+EOF
+}
 
-CGO_ENABLED=1
-go test -race -p=1 -coverprofile=cover.out ${PKGS}
+case "$1" in
+help)
+	usage
+	exit 0
+	;;
+*)
+	PKGS=${1:-./...}
+	NAME=${2:-.}
+	;;
+esac
+
+CGO_ENABLED=1 go test -race -p=1 -coverprofile=cover.out -run=$NAME $PKGS
 go tool cover -html=cover.out -o cover.html
