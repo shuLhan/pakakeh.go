efa8b6c920b41a587fee4624e7a9d0ee913a446b
Shulhan <ms@kilabit.info>
2023-05-23 00:33:02 +0700

    email/maildir: implement Folder
    Folder is a directory under maildir that store messages per file.
    A folder contains three directories: tmp, new, and cur; and an empty
    file "maildirfolder".

---
+++ lib/email/maildir/folder.go
@@ -0,0 +1,253 @@
+// Copyright 2023, Shulhan <ms@kilabit.info>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package maildir
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"unicode"
+)
+
+// An empty file name inside folder to indicate that the directory is a
+// folder.
+const fileMaildirFolder = `maildirfolder`
+
+// Folder is a directory under maildir that store messages per file.
+// A folder contains three directories: tmp, new, and cur; and an empty
+// file "maildirfolder" to indicate a directory is a folder.
+type Folder struct {
+	name   string // The base name of folder.
+	dir    string // The full path of folder inside maildir.
+	dirCur string
+	dirNew string
+	dirTmp string
+}
+
+// CreateFolder create folder under directory maildir, populate all required
+// sub directories and file.
+// A folder must start with dot '.' and does not contains unicode control
+// character.
+func CreateFolder(maildir, name string) (folder *Folder, err error) {
+	var logp = `CreateFolder`
+
+	folder = &Folder{}
+
+	folder.name, err = sanitizeFolderName(name)
+	if err != nil {
+		return nil, fmt.Errorf(`%s: %w`, logp, err)
+	}
+
+	maildir = filepath.Clean(maildir)
+	if maildir == `.` {
+		return nil, fmt.Errorf(`%s: invalid maildir %q`, logp, maildir)
+	}
+
+	folder.dir = filepath.Join(maildir, name)
+
+	err = os.Mkdir(folder.dir, 0700)
+	if err != nil && !errors.Is(err, os.ErrExist) {
+		return nil, fmt.Errorf(`%s: %w`, logp, err)
+	}
+
+	err = folder.initDirs()
+	if err != nil {
+		return nil, fmt.Errorf(`%s: %w`, logp, err)
+	}
+
+	var fileMdfolder = filepath.Join(folder.dir, fileMaildirFolder)
+
+	err = os.WriteFile(fileMdfolder, nil, 0600)
+	if err != nil {
+		return nil, fmt.Errorf(`%s: %w`, logp, err)
+	}
+
+	return folder, nil
+}
+
+// NewFolder initialize folder from directory maildir.
+// It will return an error if the one of the directory is not exist or does
+// not have permission to write, or "maildirfolder" file is not exist.
+func NewFolder(maildir, name string) (folder *Folder, err error) {
+	var logp = `NewFolder`
+
+	name = strings.TrimSpace(name)
+	if len(name) == 0 {
+		return nil, fmt.Errorf(`%s: empty folder name`, logp)
+	}
+
+	folder = &Folder{
+		name: name,
+		dir:  filepath.Join(maildir, name),
+	}
+
+	folder.dirCur = filepath.Join(folder.dir, maildirCur)
+	folder.dirNew = filepath.Join(folder.dir, maildirNew)
+	folder.dirTmp = filepath.Join(folder.dir, maildirTmp)
+
+	var fileMdfolder = filepath.Join(folder.dir, fileMaildirFolder)
+	_, err = os.Stat(fileMdfolder)
+	if err != nil {
+		return nil, fmt.Errorf(`%s: %w`, logp, err)
+	}
+	err = checkDir(folder.dirCur)
+	if err != nil {
+		return nil, fmt.Errorf(`%s: %s: %w`, logp, folder.dirCur, err)
+	}
+	err = checkDir(folder.dirNew)
+	if err != nil {
+		return nil, fmt.Errorf(`%s: %s: %w`, logp, folder.dirNew, err)
+	}
+	err = checkDir(folder.dirTmp)
+	if err != nil {
+		return nil, fmt.Errorf(`%s: %s: %w`, logp, folder.dirTmp, err)
+	}
+
+	return folder, nil
+}
+
+// Delete hard delete a message file in "cur".
+// It will return no error if the file does not exist.
+func (folder *Folder) Delete(file string) (err error) {
+	file = strings.TrimSpace(file)
+	if len(file) == 0 {
+		// Prevent removing the cur directory.
+		return nil
+	}
+
+	var (
+		logp = `Delete`
+		fdel = filepath.Join(folder.dirCur, file)
+	)
+
+	err = os.Remove(fdel)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil
+		}
+		return fmt.Errorf(`%s: %w`, logp, err)
+	}
+	return nil
+}
+
+// Fetch fetch the content of file from folder "new" directory.
+// If the file exist, move it to the "cur" and add suffix ":2" to the file
+// name.
+// If the file does not exist, it will return nil without an error.
+func (folder *Folder) Fetch(file string) (fileCur string, msg []byte, err error) {
+	file = strings.TrimSpace(file)
+	if len(file) == 0 {
+		return ``, nil, nil
+	}
+
+	fileCur = file + `:2`
+
+	var (
+		logp    = `Fetch`
+		pathNew = filepath.Join(folder.dirNew, file)
+		pathCur = filepath.Join(folder.dirCur, fileCur)
+	)
+
+	msg, err = os.ReadFile(pathNew)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return ``, nil, nil
+		}
+		return ``, nil, fmt.Errorf(`%s: %w`, logp, err)
+	}
+
+	err = os.Rename(pathNew, pathCur)
+	if err != nil {
+		return ``, nil, fmt.Errorf(`%s: %w`, logp, err)
+	}
+
+	return fileCur, msg, nil
+}
+
+// Get the content of file from "cur" directory.
+// It will return nil without an error if file is not exist.
+func (folder *Folder) Get(file string) (msg []byte, err error) {
+	var (
+		logp    = `Get`
+		pathCur = filepath.Join(folder.dirCur, file)
+	)
+	msg, err = os.ReadFile(pathCur)
+	if err != nil && !errors.Is(err, os.ErrNotExist) {
+		return nil, fmt.Errorf(`%s: %w`, logp, err)
+	}
+	return msg, nil
+}
+
+func (folder *Folder) initDirs() (err error) {
+	var logp = `initDirs`
+
+	folder.dirCur = filepath.Join(folder.dir, maildirCur)
+	err = os.Mkdir(folder.dirCur, 0700)
+	if err != nil && !errors.Is(err, os.ErrExist) {
+		return fmt.Errorf(`%s: %s`, logp, err)
+	}
+
+	folder.dirNew = filepath.Join(folder.dir, maildirNew)
+	err = os.Mkdir(folder.dirNew, 0700)
+	if err != nil && !errors.Is(err, os.ErrExist) {
+		return fmt.Errorf(`%s: %s`, logp, err)
+	}
+
+	folder.dirTmp = filepath.Join(folder.dir, maildirTmp)
+	err = os.Mkdir(folder.dirTmp, 0700)
+	if err != nil && !errors.Is(err, os.ErrExist) {
+		return fmt.Errorf(`%s: %s`, logp, err)
+	}
+
+	return nil
+}
+
+// checkDir check if dir is exist and have permission to write.
+func checkDir(dir string) (err error) {
+	var fi os.FileInfo
+
+	fi, err = os.Stat(dir)
+	if err != nil {
+		return err
+	}
+
+	var perm = fi.Mode().Perm()
+	if perm&0700 != 0700 {
+		// No permission to write and enter the directory.
+		return os.ErrPermission
+	}
+
+	return nil
+}
+
+// sanitizeFolderName check if the folder name is not empty, begin with
+// period '.' and does not contains unicode control characters.
+func sanitizeFolderName(name string) (out string, err error) {
+	out = strings.TrimSpace(name)
+	if len(out) == 0 {
+		return ``, fmt.Errorf(`folder name is empty`)
+	}
+	if len(out) == 1 && out[0] == '.' {
+		return ``, fmt.Errorf(`folder name is empty`)
+	}
+	if out[0] != '.' {
+		return ``, fmt.Errorf(`folder name must begin with period`)
+	}
+	if out[1] == '.' {
+		return ``, fmt.Errorf(`folder name must not begin with ..`)
+	}
+	var r rune
+	for _, r = range out {
+		if !unicode.IsPrint(r) {
+			return ``, fmt.Errorf(`folder name contains unprintable character %q`, r)
+		}
+		if r == '/' {
+			return ``, fmt.Errorf(`folder name must not contains slash '/'`)
+		}
+	}
+	return out, nil
+}
---
+++ lib/email/maildir/folder_test.go
@@ -0,0 +1,369 @@
+// Copyright 2023, Shulhan <ms@kilabit.info>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package maildir
+
+import (
+	"errors"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/shuLhan/share/lib/test"
+)
+
+func TestCreateFolder(t *testing.T) {
+	var (
+		dir  = t.TempDir()
+		name = "  \r\n"
+
+		folder *Folder
+		err    error
+	)
+
+	// Case: with empty maildir.
+
+	_, err = CreateFolder(``, `.name`)
+	var expError = `CreateFolder: invalid maildir "."`
+	test.Assert(t, `With empty maildir`, expError, err.Error())
+
+	// Case: with empty name.
+
+	_, err = CreateFolder(dir, name)
+	expError = `CreateFolder: folder name is empty`
+	test.Assert(t, `With empty name`, expError, err.Error())
+
+	// Case: with no permission.
+
+	name = `.folder`
+
+	_, err = CreateFolder(`/`, name)
+	expError = `CreateFolder: mkdir /.folder: permission denied`
+	test.Assert(t, `With empty name`, expError, err.Error())
+
+	// Case: with valid dir and name.
+
+	folder, err = CreateFolder(dir, name)
+	if err != nil {
+		t.Fatal(err)
+	}
+	var expDir = filepath.Join(dir, name)
+	test.Assert(t, `Folder.dir`, expDir, folder.dir)
+	assertFolder(t, folder)
+
+	// Case: creating folder on existing non empty directory should not
+	// failed.
+
+	folder, err = CreateFolder(dir, name)
+	if err != nil {
+		t.Fatal(err)
+	}
+	test.Assert(t, `Folder.dir`, expDir, folder.dir)
+	assertFolder(t, folder)
+}
+
+func TestNewFolder(t *testing.T) {
+	// Case: empty name.
+	var (
+		dir      = t.TempDir()
+		name     = " \r\n"
+		expError = `NewFolder: empty folder name`
+
+		folder *Folder
+		err    error
+	)
+
+	_, err = NewFolder(dir, name)
+	test.Assert(t, `With empty name`, expError, err.Error())
+
+	// Case: with no "maildirfolder".
+	name = `.folder`
+	_, err = NewFolder(dir, name)
+	if !errors.Is(err, os.ErrNotExist) {
+		t.Fatalf(`want error %q, got %q`, os.ErrNotExist, err)
+	}
+
+	// Case: with no "cur" folder.
+	name = `.maildir_without_cur`
+
+	folder, err = CreateFolder(dir, name)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = os.Remove(folder.dirCur)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = NewFolder(dir, name)
+	if !errors.Is(err, os.ErrNotExist) {
+		t.Fatalf(`want error %s, got %s`, os.ErrNotExist, err)
+	}
+
+	// Case: with invalid "tmp" folder permission.
+	name = `.maildir_with_tmp_permission`
+
+	folder, err = CreateFolder(dir, name)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = os.Chmod(folder.dirTmp, 0500)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = NewFolder(dir, name)
+	if !errors.Is(err, os.ErrPermission) {
+		t.Fatalf(`want error %s, got %s`, os.ErrPermission, err)
+	}
+
+	// Case: success.
+	name = `.folder`
+
+	_, err = CreateFolder(dir, name)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	folder, err = NewFolder(dir, name)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = checkDir(folder.dirCur)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = checkDir(folder.dirNew)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = checkDir(folder.dirTmp)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestFolder(t *testing.T) {
+	var (
+		dir    = t.TempDir()
+		name   = `.folder`
+		folder *Folder
+		err    error
+	)
+
+	folder, err = CreateFolder(dir, name)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Run(`Delete`, func(tt *testing.T) {
+		testFolderDelete(tt, folder)
+	})
+	t.Run(`Fetch`, func(tt *testing.T) {
+		testFolderFetch(tt, folder)
+	})
+	t.Run(`Get`, func(tt *testing.T) {
+		testFolderGet(tt, folder)
+	})
+}
+
+func TestSanitizeFolderName(t *testing.T) {
+	type testCase struct {
+		name     string
+		expError string
+		exp      string
+	}
+	var cases = []testCase{{
+		name:     ``,
+		expError: `folder name is empty`,
+	}, {
+		name:     `.`,
+		expError: `folder name is empty`,
+	}, {
+		name:     `notperiod`,
+		expError: `folder name must begin with period`,
+	}, {
+		name:     `..`,
+		expError: `folder name must not begin with ..`,
+	}, {
+		name:     `..name`,
+		expError: `folder name must not begin with ..`,
+	}, {
+		name:     ".\u0001name",
+		expError: `folder name contains unprintable character '\x01'`,
+	}, {
+		name:     ".\rname",
+		expError: `folder name contains unprintable character '\r'`,
+	}, {
+		name:     ".\nname",
+		expError: `folder name contains unprintable character '\n'`,
+	}, {
+		name:     ".na/me",
+		expError: `folder name must not contains slash '/'`,
+	}, {
+		name: ".name\n", // The space is trimmed.
+		exp:  `.name`,
+	}, {
+		name: `.folder.sub`,
+		exp:  `.folder.sub`,
+	}}
+	var (
+		c        testCase
+		gotError string
+		got      string
+		err      error
+	)
+	for _, c = range cases {
+		got, err = sanitizeFolderName(c.name)
+		if err != nil {
+			gotError = err.Error()
+		}
+		test.Assert(t, c.name, c.expError, gotError)
+		test.Assert(t, c.name, c.exp, got)
+		gotError = ``
+	}
+}
+
+func testFolderDelete(t *testing.T, folder *Folder) {
+	// Case: name is empty.
+	var (
+		name string
+		err  error
+	)
+	err = folder.Delete(name)
+	if err != nil {
+		t.Fatalf(`want no error, got %s`, err)
+	}
+
+	// Case: file not exist.
+	name = `filenotexist`
+	err = folder.Delete(name)
+	if err != nil {
+		t.Fatalf(`want no error, got %s`, err)
+	}
+
+	// Case: file exist.
+	name = `a:2`
+	var fileCur = filepath.Join(folder.dirCur, name)
+
+	err = os.WriteFile(fileCur, nil, 0600)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = folder.Delete(name)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = os.Stat(fileCur)
+	if !errors.Is(err, os.ErrNotExist) {
+		t.Fatalf(`want error %q, got %q`, os.ErrNotExist, err)
+	}
+}
+
+func testFolderFetch(t *testing.T, folder *Folder) {
+	// Case: name is empty.
+	var (
+		name string
+		err  error
+	)
+	_, _, err = folder.Fetch(name)
+	if err != nil {
+		t.Fatalf(`want no error, got %s`, err)
+	}
+
+	// Case: file not exist.
+	name = `filenotexist`
+	_, _, err = folder.Fetch(name)
+	if err != nil {
+		t.Fatalf(`want no error, got %s`, err)
+	}
+
+	// Case: file exist.
+	name = `a`
+	var (
+		fileNew = filepath.Join(folder.dirNew, name)
+		msg     = []byte(`content of file`)
+
+		fileCur string
+	)
+
+	err = os.WriteFile(fileNew, msg, 0600)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fileCur, msg, err = folder.Fetch(name)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = os.Stat(fileNew)
+	if !errors.Is(err, os.ErrNotExist) {
+		t.Fatalf(`want error %q, got %q`, os.ErrNotExist, err)
+	}
+
+	test.Assert(t, `content`, `content of file`, string(msg))
+	test.Assert(t, `fileCur`, `a:2`, fileCur)
+}
+
+func testFolderGet(t *testing.T, folder *Folder) {
+	// Case: file not exist.
+	var (
+		name = `filenotexist`
+
+		expMsg []byte
+		gotMsg []byte
+		err    error
+	)
+
+	gotMsg, err = folder.Get(name)
+	if err != nil {
+		t.Fatalf(`want no error, got %s`, err)
+	}
+	test.Assert(t, `msg`, expMsg, gotMsg)
+
+	// Case: file exist.
+	name = `a:2`
+	expMsg = []byte(`content of file`)
+
+	var fileCur = filepath.Join(folder.dirNew, name)
+
+	err = os.WriteFile(fileCur, expMsg, 0600)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	gotMsg, err = folder.Get(name)
+	if err != nil {
+		t.Fatal(err)
+	}
+	test.Assert(t, `content`, string(expMsg), string(gotMsg))
+}
+
+func assertFolder(t *testing.T, folder *Folder) {
+	var err error
+
+	_, err = os.Stat(folder.dirCur)
+	if err != err {
+		t.Fatalf(`want %s, got %s`, folder.dirCur, err)
+	}
+	_, err = os.Stat(folder.dirNew)
+	if err != err {
+		t.Fatalf(`want %s, got %s`, folder.dirNew, err)
+	}
+	_, err = os.Stat(folder.dirTmp)
+	if err != err {
+		t.Fatalf(`want %s, got %s`, folder.dirTmp, err)
+	}
+
+	var fileMd = filepath.Join(folder.dir, fileMaildirFolder)
+
+	_, err = os.Stat(fileMd)
+	if err != nil {
+		t.Fatalf(`want %s, got %s`, fileMd, err)
+	}
+}
