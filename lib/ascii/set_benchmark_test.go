78a5d4bd63946977725f8540eeff7953d3b36039
Shulhan <ms@kilabit.info>
2023-06-18 13:08:35 +0700

    lib/ascii: add unit and benchmark tests
    The unit and benchmark tests are taken from upstream [1], with author
    permission on this GitHub comment [2].
    
    [1] https://github.com/elliotwutingfeng/asciiset
    [2] https://github.com/shuLhan/share/pull/7#issuecomment-1573271518

---
+++ lib/ascii/set_benchmark_test.go
@@ -0,0 +1,187 @@
+// Copyright (c) 2022, Wu Tingfeng <wutingfeng@outlook.com>
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package ascii
+
+import (
+	"testing"
+	"unicode/utf8"
+)
+
+// prevent compiler from erroneously optimising benchmark computations
+var globalExists bool
+var globalSize int
+var globalVal byte
+
+// Number of sets
+const N int = 10
+
+// setupSets returns an Set slice of size n where
+// each Set is empty
+//
+// if populate is true, fill each set with every 2nd ASCII character
+func setupSets(n int, populate bool) []Set {
+	sets := []Set{}
+	for i := 0; i < n; i++ {
+		var as Set
+		if populate {
+			for c := 0; c < utf8.RuneSelf; c += 2 {
+				// add every 2nd ASCII character
+				as.Add(byte(c))
+			}
+		}
+		sets = append(sets, as)
+	}
+	return sets
+}
+
+// setupMapSets returns a map[byte]struct{} slice of size n where
+// each map[byte]struct{} is empty
+//
+// if populate is true, fill each set with every 2nd ASCII character
+func setupMapSets(n int, populate bool) []map[byte]struct{} {
+	sets := []map[byte]struct{}{}
+	for i := 0; i < n; i++ {
+		as := make(map[byte]struct{})
+		if populate {
+			for c := 0; c < utf8.RuneSelf; c += 2 {
+				// add every 2nd ASCII character
+				as[byte(c)] = struct{}{}
+			}
+		}
+		sets = append(sets, as)
+	}
+	return sets
+}
+
+func BenchmarkSet(b *testing.B) {
+	b.Run("Set Add()", func(b *testing.B) {
+		sets := setupSets(N, false)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			for _, as := range sets {
+				for c := byte(0); c < utf8.RuneSelf; c += 2 {
+					// add every 2nd ASCII character
+					as.Add(c)
+				}
+			}
+		}
+	})
+	b.Run("Set Contains()", func(b *testing.B) {
+		sets := setupSets(N, true)
+		var exists bool
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			for _, as := range sets {
+				for c := byte(0); c < utf8.RuneSelf; c++ {
+					exists = as.Contains(c)
+				}
+			}
+		}
+		globalExists = exists
+	})
+	b.Run("Set Remove()", func(b *testing.B) {
+		sets := setupSets(N, true)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			for _, as := range sets {
+				for c := byte(0); c < utf8.RuneSelf; c++ {
+					as.Remove(c)
+				}
+			}
+		}
+	})
+	b.Run("Set Size()", func(b *testing.B) {
+		sets := setupSets(N, true)
+		var size int
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			for _, as := range sets {
+				for c := byte(0); c < utf8.RuneSelf; c++ {
+					size = as.Size()
+				}
+			}
+		}
+		globalSize = size
+	})
+	b.Run("Set Visit()", func(b *testing.B) {
+		sets := setupSets(N, true)
+		var val byte
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			for _, as := range sets {
+				as.Visit(func(c byte) bool {
+					val = c
+					return false
+				})
+			}
+		}
+		globalVal = val
+	})
+}
+
+func BenchmarkMapSet(b *testing.B) {
+	b.Run("map Add", func(b *testing.B) {
+		sets := setupMapSets(N, false)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			for _, as := range sets {
+				for c := byte(0); c < utf8.RuneSelf; c += 2 {
+					// add every 2nd ASCII character
+					as[c] = struct{}{}
+				}
+			}
+		}
+	})
+	b.Run("map Contains", func(b *testing.B) {
+		sets := setupMapSets(N, true)
+		var exists bool
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			for _, as := range sets {
+				for c := byte(0); c < utf8.RuneSelf; c++ {
+					_, exists = as[c]
+				}
+			}
+		}
+		globalExists = exists
+	})
+	b.Run("map Remove", func(b *testing.B) {
+		sets := setupMapSets(N, true)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			for _, as := range sets {
+				for c := byte(0); c < utf8.RuneSelf; c++ {
+					delete(as, c)
+				}
+			}
+		}
+	})
+	b.Run("map Size", func(b *testing.B) {
+		sets := setupMapSets(N, true)
+		var size int
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			for _, as := range sets {
+				for c := byte(0); c < utf8.RuneSelf; c++ {
+					size = len(as)
+				}
+			}
+		}
+		globalSize = size
+	})
+	b.Run("map Visit", func(b *testing.B) {
+		sets := setupMapSets(N, true)
+		var val byte
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			for _, as := range sets {
+				for c := range as {
+					val = c
+				}
+			}
+		}
+		globalVal = val
+	})
+}
---
+++ lib/ascii/set_test.go
@@ -0,0 +1,191 @@
+// Copyright (c) 2022, Wu Tingfeng <wutingfeng@outlook.com>
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package ascii
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestMakeSet(t *testing.T) {
+	if _, ok := MakeSet("@J"); !ok {
+		t.Errorf(`MakeSet should identify "@J" as all ASCII`)
+	}
+	if _, ok := MakeSet("@\u00f8"); ok {
+		t.Errorf(`MakeSet should identify "@\u00f8" as not all ASCII`)
+	}
+}
+
+func TestAdd(t *testing.T) {
+	chars := "@J"
+	var as Set
+	for _, c := range chars {
+		as.Add(byte(c))
+		if !as.Contains(byte(c)) {
+			t.Errorf("Set should contain %s", string(c))
+		}
+	}
+	britishPound := byte('£')
+	as.Add(britishPound)
+	if as.Contains(britishPound) {
+		t.Errorf("Set should not contain %s", string(britishPound))
+	}
+}
+
+func TestContains(t *testing.T) {
+	as, _ := MakeSet("@J")
+	if as.Contains('5') {
+		t.Errorf("Set should not contain 5")
+	}
+}
+
+func TestRemove(t *testing.T) {
+	chars := "@J"
+	as, _ := MakeSet(chars)
+	as.Remove('@')
+	if as.Contains('@') {
+		t.Errorf("Set should not contain @")
+	}
+	if !as.Contains('J') {
+		t.Errorf("Set should contain J")
+	}
+}
+
+func TestSize(t *testing.T) {
+	as, _ := MakeSet("ABCD")
+	if size := as.Size(); size != 4 {
+		t.Errorf("Expected Size 4, got %d", size)
+	}
+}
+
+func TestUnion(t *testing.T) {
+	as, _ := MakeSet("ABCD")
+	as2, _ := MakeSet("CDEF")
+	as3 := as.Union(as2)
+	for _, c := range "ABCDEF" {
+		if !as3.Contains(byte(c)) {
+			t.Errorf("Set should contain %s", string(c))
+		}
+	}
+}
+
+func TestIntersection(t *testing.T) {
+	as, _ := MakeSet("ABCD")
+	as2, _ := MakeSet("CDEF")
+	as3 := as.Intersection(as2)
+	for _, c := range "CD" {
+		if !as3.Contains(byte(c)) {
+			t.Errorf("Set should contain %s", string(c))
+		}
+	}
+	for _, c := range "ABEF" {
+		if as3.Contains(byte(c)) {
+			t.Errorf("Set should not contain %s", string(c))
+		}
+	}
+}
+
+func TestSubtract(t *testing.T) {
+	as, _ := MakeSet("ABCD")
+	as2, _ := MakeSet("CDEF")
+	as3 := as.Subtract(as2)
+	for _, c := range "AB" {
+		if !as3.Contains(byte(c)) {
+			t.Errorf("Set should contain %s", string(c))
+		}
+	}
+	for _, c := range "CDEF" {
+		if as3.Contains(byte(c)) {
+			t.Errorf("Set should not contain %s", string(c))
+		}
+	}
+}
+
+func TestEquals(t *testing.T) {
+	as, _ := MakeSet("ABCD")
+	as2, _ := MakeSet("ABCD")
+	if !as.Equal(as2) {
+		t.Errorf("as should be equal to as2")
+	}
+}
+
+func TestVisit(t *testing.T) {
+	// chars must be all unique and in ascending order
+	chars := "123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	as, _ := MakeSet(chars)
+
+	// scenario: visit every character in the set
+	output := make([]byte, 0, len(chars))
+	as.Visit(func(n byte) bool {
+		if as.Contains(n) {
+			output = append(output, n)
+		}
+		return false
+	})
+	if len(output) != len(chars) {
+		t.Errorf("output length must be %d; visit every character. Got %d", len(chars), len(output))
+	}
+	for i := 0; i < len(chars); i++ {
+		if output[i] != byte(chars[i]) {
+			t.Errorf("%d %d", output[i], byte(chars[i]))
+		}
+	}
+	// scenario: stop early at 'T'
+	output = make([]byte, 0, strings.Index(chars, "T")+1)
+	as.Visit(func(n byte) bool {
+		if as.Contains(n) {
+			output = append(output, n)
+		}
+		return n == 'T'
+	})
+	if len(output) != strings.Index(chars, "T")+1 {
+		t.Errorf("output length must be %d; stop early at 'T'. Got %d", strings.Index(chars, "T")+1, len(output))
+	}
+	for i := 0; i < strings.Index(chars, "T")+1; i++ {
+		if output[i] != byte(chars[i]) {
+			t.Errorf("%d %d", output[i], byte(chars[i]))
+		}
+	}
+	// scenario: Add extra '\n'
+	output = make([]byte, 0, len(chars))
+	as.Visit(func(n byte) bool {
+		as.Add('\n')
+		if as.Contains(n) {
+			output = append(output, n)
+		}
+		return false
+	})
+	if as.Size() != len(chars)+1 {
+		t.Errorf("as.Size() must be %d; visit every character and add extra '\n'. Got %d", len(chars)+1, as.Size())
+	}
+	if len(output) != len(chars) {
+		t.Errorf("output length must be %d; visit every character and add extra '\n'. Got %d", len(chars), len(output))
+	}
+	for i := 0; i < len(chars); i++ {
+		if output[i] != byte(chars[i]) {
+			t.Errorf("%d %d", output[i], byte(chars[i]))
+		}
+	}
+	// scenario: Remove extra '\n'
+	output = make([]byte, 0, len(chars))
+	as.Visit(func(n byte) bool {
+		as.Remove('\n')
+		if as.Contains(n) {
+			output = append(output, n)
+		}
+		return false
+	})
+	if as.Size() != len(chars) {
+		t.Errorf("as.Size() must be %d; visit every character and remove extra '\n'. Got %d", len(chars), as.Size())
+	}
+	if len(output) != len(chars) {
+		t.Errorf("output length must be %d; visit every character and remove extra '\n'. Got %d", len(chars), len(output))
+	}
+	for i := 0; i < len(chars); i++ {
+		if output[i] != byte(chars[i]) {
+			t.Errorf("%d %d", output[i], byte(chars[i]))
+		}
+	}
+}
