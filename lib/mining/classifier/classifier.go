45fc440a3f3a7dacfe7141308e099d726d9a04d1
Shulhan <ms@kilabit.info>
2023-09-10 22:42:00 +0700

    lib/mining: add package comment for mining and classifier

---
+++ lib/mining/classifier/classifier.go
@@ -0,0 +1,7 @@
+// Copyright 2016 Mhd Sulhan <ms@kilabit.info>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package classifier provides machine learning classifier library,
+// including CART, Random Forest, Cascaded Random Forest, and KNN.
+package classifier
---
+++ lib/mining/mining.go
@@ -0,0 +1,23 @@
+// Copyright 2018 Mhd Sulhan <ms@kilabit.info>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package mining provides library for data mining and statistic related to
+// machine learning.
+//
+// Classifiers,
+//
+//   - CART
+//   - Random Forest
+//   - Cascaded Random Forest
+//   - K-Nearest Neighbourhood
+//
+// Resampling,
+//
+//   - SMOTE
+//   - LN-SMOTE (Local Neigbourhood SMOTE)
+//
+// Miscellaneous,
+//
+//   - Gini index
+package mining
