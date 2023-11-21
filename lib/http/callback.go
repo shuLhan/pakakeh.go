b6f055e1a1612a99cba3fd11a1060b41a730ec91
Shulhan <ms@kilabit.info>
2023-11-22 03:23:46 +0700

    lib/http: update comment on Callback type

--- lib/http/callback.go
+++ lib/http/callback.go
@@ -7,14 +7,14 @@ package http
 // Callback define a type of function for handling registered handler.
 //
 // The function will have the query URL, request multipart form data,
-// and request body ready to be used in EndpointRequest.HttpRequest and
-// EndpointRequest.RequestBody fields.
+// and request body ready to be used in [EndpointRequest.HttpRequest] and
+// [EndpointRequest.RequestBody] fields.
 //
-// The EndpointRequest.HttpWriter can be used to write custom header or to
+// The [EndpointRequest.HttpWriter] can be used to write custom header or to
 // write cookies but should not be used to write response body.
 //
 // The error return type should be an instance of *errors.E, with E.Code
 // define the HTTP status code.
-// If error is not nil and not *errors.E, server will response with
-// internal-server-error status code.
+// If error is not nil and not *[liberrors.E], server will response with
+// [http.StatusInternalServerError] status code.
 type Callback func(req *EndpointRequest) (resBody []byte, err error)
