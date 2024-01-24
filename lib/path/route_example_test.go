d686dc52c221360295c4b48a987329abe624e4fa
Shulhan <ms@kilabit.info>
2024-01-25 00:48:28 +0700

    lib/path: add example for [Route.Parse]

--- lib/path/route_example_test.go
+++ lib/path/route_example_test.go
@@ -11,6 +11,37 @@ import (
 	libpath "github.com/shuLhan/share/lib/path"
 )
 
+func ExampleRoute_Parse() {
+	var (
+		rute *libpath.Route
+		err  error
+	)
+
+	rute, err = libpath.NewRoute(`/book/:title/:page`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var (
+		vals map[string]string
+		ok   bool
+	)
+
+	vals, ok = rute.Parse(`/book/Hitchiker to Galaxy/42`)
+	fmt.Println(ok, vals)
+
+	vals, ok = rute.Parse(`/book/Hitchiker to Galaxy`)
+	fmt.Println(ok, vals)
+
+	vals, ok = rute.Parse(`/book/Hitchiker to Galaxy/42/order`)
+	fmt.Println(ok, vals)
+
+	// Output:
+	// true map[page:42 title:hitchiker to galaxy]
+	// false map[]
+	// false map[]
+}
+
 func ExampleRoute_Set() {
 	var (
 		rute *libpath.Route
