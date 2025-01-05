a336d86c5bc20c99ffdd40cb8521357f009f6a67
Shulhan <ms@kilabit.info>
2025-01-06 00:51:44 +0700

    lib/memfs: remove GoEmbed test that generate dynamic times
    The GoEmbed test that write Go embedded code into "internal/test/embed/"
    may create unnecessary changes when the test run on new clone of this
    repository, or when we run "go test" again on that package after rebasing.

--- lib/memfs/embed_test.go
+++ lib/memfs/embed_test.go
@@ -1,39 +1,13 @@
-// Copyright 2019, Shulhan <ms@kilabit.info>. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// SPDX-FileCopyrightText: 2019 M. Shulhan <ms@kilabit.info>
+// SPDX-License-Identifier: BSD-3-Clause
 
 package memfs
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestMemFS_GoEmbed(t *testing.T) {
-	opts := &Options{
-		Root: "testdata",
-		Excludes: []string{
-			`^\..*`,
-			".*/node_save$",
-		},
-		Embed: EmbedOptions{
-			CommentHeader: `// SPDX-FileCopyrightText: 2022 Shulhan <ms@kilabit.info>
-// SPDX-License-Identifier: AGPL-3.0-or-later
-`,
-			PackageName: "embed",
-			GoFileName:  "./internal/test/embed/embed_test.go",
-		},
-	}
-
-	mfs, err := New(opts)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	err = mfs.GoEmbed()
-	if err != nil {
-		t.Fatal(err)
-	}
-}
-
-func TestMemFS_GoEmbed_DisableModTime(t *testing.T) {
+func TestMemFS_GoEmbed_WithoutModTime(t *testing.T) {
 	opts := &Options{
 		Root: "testdata",
 		Excludes: []string{
